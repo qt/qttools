11fdb424ba8597eda9680a6a188f444b7ed93d68
Stephen Kelly <stephen.kelly@kdab.com>
2011-12-19 22:35:19 +0100

    Fix CMake style.
    Change-Id: I4ec793889a7f6564938203766737c957f2044a83
    Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Alexander Neundorf <neundorf@kde.org>
    Reviewed-by: Stephen Kelly <stephen.kelly@kdab.com>

--- src/qdbus/Qt5DBusToolsMacros.cmake
+++ src/qdbus/Qt5DBusToolsMacros.cmake
@@ -32,130 +32,129 @@
 
 include(MacroAddFileDependencies)
 
-MACRO (QT5_EXTRACT_OPTIONS _qt5_files _qt5_options)
-  SET(${_qt5_files})
-  SET(${_qt5_options})
-  SET(_QT5_DOING_OPTIONS FALSE)
-  FOREACH(_currentArg ${ARGN})
-    IF ("${_currentArg}" STREQUAL "OPTIONS")
-      SET(_QT5_DOING_OPTIONS TRUE)
-    ELSE ("${_currentArg}" STREQUAL "OPTIONS")
-      IF(_QT5_DOING_OPTIONS)
-        LIST(APPEND ${_qt5_options} "${_currentArg}")
-      ELSE(_QT5_DOING_OPTIONS)
-        LIST(APPEND ${_qt5_files} "${_currentArg}")
-      ENDIF(_QT5_DOING_OPTIONS)
-    ENDIF ("${_currentArg}" STREQUAL "OPTIONS")
-  ENDFOREACH(_currentArg)
-ENDMACRO (QT5_EXTRACT_OPTIONS)
+macro(QT5_EXTRACT_OPTIONS _qt5_files _qt5_options)
+    set(${_qt5_files})
+    set(${_qt5_options})
+    set(_QT5_DOING_OPTIONS FALSE)
+    foreach(_currentArg ${ARGN})
+        if("${_currentArg}" STREQUAL "OPTIONS")
+            set(_QT5_DOING_OPTIONS TRUE)
+        else()
+            if(_QT5_DOING_OPTIONS)
+                list(APPEND ${_qt5_options} "${_currentArg}")
+            else()
+                list(APPEND ${_qt5_files} "${_currentArg}")
+            endif()
+        endif()
+    endforeach()
+endmacro()
 
 
-MACRO(QT5_ADD_DBUS_INTERFACE _sources _interface _basename)
-  GET_FILENAME_COMPONENT(_infile ${_interface} ABSOLUTE)
-  SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
-  SET(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
-  SET(_moc    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
+macro(QT5_ADD_DBUS_INTERFACE _sources _interface _basename)
+    get_filename_component(_infile ${_interface} ABSOLUTE)
+    set(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
+    set(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+    set(_moc    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
 
-  GET_SOURCE_FILE_PROPERTY(_nonamespace ${_interface} NO_NAMESPACE)
-  IF(_nonamespace)
-    SET(_params -N -m)
-  ELSE(_nonamespace)
-    SET(_params -m)
-  ENDIF(_nonamespace)
-
-  GET_SOURCE_FILE_PROPERTY(_classname ${_interface} CLASSNAME)
-  IF(_classname)
-    SET(_params ${_params} -c ${_classname})
-  ENDIF(_classname)
-
-  GET_SOURCE_FILE_PROPERTY(_include ${_interface} INCLUDE)
-  IF(_include)
-    SET(_params ${_params} -i ${_include})
-  ENDIF(_include)
-
-  ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
-      COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} ${_params} -p ${_basename} ${_infile}
-      DEPENDS ${_infile} VERBATIM)
-
-  SET_SOURCE_FILES_PROPERTIES(${_impl} PROPERTIES SKIP_AUTOMOC TRUE)
-
-  QT5_GENERATE_MOC(${_header} ${_moc})
-
-  SET(${_sources} ${${_sources}} ${_impl} ${_header} ${_moc})
-  MACRO_ADD_FILE_DEPENDENCIES(${_impl} ${_moc})
-
-ENDMACRO(QT5_ADD_DBUS_INTERFACE)
-
-
-MACRO(QT5_ADD_DBUS_INTERFACES _sources)
-  FOREACH (_current_FILE ${ARGN})
-    GET_FILENAME_COMPONENT(_infile ${_current_FILE} ABSOLUTE)
-    # get the part before the ".xml" suffix
-    STRING(REGEX REPLACE "(.*[/\\.])?([^\\.]+)\\.xml" "\\2" _basename ${_current_FILE})
-    STRING(TOLOWER ${_basename} _basename)
-    QT5_ADD_DBUS_INTERFACE(${_sources} ${_infile} ${_basename}interface)
-  ENDFOREACH (_current_FILE)
-ENDMACRO(QT5_ADD_DBUS_INTERFACES)
-
-
-MACRO(QT5_GENERATE_DBUS_INTERFACE _header) # _customName OPTIONS -some -options )
-  QT5_EXTRACT_OPTIONS(_customName _qt4_dbus_options ${ARGN})
-
-  GET_FILENAME_COMPONENT(_in_file ${_header} ABSOLUTE)
-  GET_FILENAME_COMPONENT(_basename ${_header} NAME_WE)
-
-  IF (_customName)
-    if (IS_ABSOLUTE ${_customName})
-      get_filename_component(_containingDir ${_customName} PATH)
-      if (NOT EXISTS ${_containingDir})
-        file(MAKE_DIRECTORY "${_containingDir}")
-      endif()
-      SET(_target ${_customName})
+    get_source_file_property(_nonamespace ${_interface} NO_NAMESPACE)
+    if(_nonamespace)
+        set(_params -N -m)
     else()
-      SET(_target ${CMAKE_CURRENT_BINARY_DIR}/${_customName})
+        set(_params -m)
     endif()
-  ELSE (_customName)
-    SET(_target ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.xml)
-  ENDIF (_customName)
 
-  ADD_CUSTOM_COMMAND(OUTPUT ${_target}
-      COMMAND ${QT_DBUSCPP2XML_EXECUTABLE} ${_qt4_dbus_options} ${_in_file} -o ${_target}
-      DEPENDS ${_in_file} VERBATIM
-  )
-ENDMACRO(QT5_GENERATE_DBUS_INTERFACE)
+    get_source_file_property(_classname ${_interface} CLASSNAME)
+    if(_classname)
+        set(_params ${_params} -c ${_classname})
+    endif()
+
+    get_source_file_property(_include ${_interface} INCLUDE)
+    if(_include)
+        set(_params ${_params} -i ${_include})
+    endif()
+
+    add_custom_command(OUTPUT ${_impl} ${_header}
+        COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} ${_params} -p ${_basename} ${_infile}
+        DEPENDS ${_infile} VERBATIM)
+
+    set_source_files_properties(${_impl} PROPERTIES SKIP_AUTOMOC TRUE)
+
+    qt5_generate_moc(${_header} ${_moc})
+
+    set(${_sources} ${${_sources}} ${_impl} ${_header} ${_moc})
+    macro_add_file_dependencies(${_impl} ${_moc}))
+endmacro()
 
 
-MACRO(QT5_ADD_DBUS_ADAPTOR _sources _xml_file _include _parentClass) # _optionalBasename _optionalClassName)
-  GET_FILENAME_COMPONENT(_infile ${_xml_file} ABSOLUTE)
+macro(QT5_ADD_DBUS_INTERFACES _sources)
+    foreach(_current_FILE ${ARGN})
+        get_filename_component(_infile ${_current_FILE} ABSOLUTE)
+        # get the part before the ".xml" suffix
+        string(REGEX REPLACE "(.*[/\\.])?([^\\.]+)\\.xml" "\\2" _basename ${_current_FILE})
+        string(TOLOWER ${_basename} _basename)
+        qt5_add_dbus_interface(${_sources} ${_infile} ${_basename}interface)
+    endforeach()
+endmacro()
 
-  SET(_optionalBasename "${ARGV4}")
-  IF (_optionalBasename)
-    SET(_basename ${_optionalBasename} )
-  ELSE (_optionalBasename)
-    STRING(REGEX REPLACE "(.*[/\\.])?([^\\.]+)\\.xml" "\\2adaptor" _basename ${_infile})
-    STRING(TOLOWER ${_basename} _basename)
-  ENDIF (_optionalBasename)
 
-  SET(_optionalClassName "${ARGV5}")
-  SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
-  SET(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
-  SET(_moc    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
+macro(QT5_GENERATE_DBUS_INTERFACE _header) # _customName OPTIONS -some -options )
+    qt5_extract_options(_customName _qt4_dbus_options ${ARGN})
 
-  IF(_optionalClassName)
-    ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
-       COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -c ${_optionalClassName} -i ${_include} -l ${_parentClass} ${_infile}
-       DEPENDS ${_infile} VERBATIM
+    get_filename_component(_in_file ${_header} ABSOLUTE)
+    get_filename_component(_basename ${_header} NAME_WE)
+
+    if(_customName)
+        if(IS_ABSOLUTE ${_customName})
+          get_filename_component(_containingDir ${_customName} PATH)
+          if(NOT EXISTS ${_containingDir})
+              file(MAKE_DIRECTORY "${_containingDir}")
+          endif()
+          set(_target ${_customName})
+        else()
+            set(_target ${CMAKE_CURRENT_BINARY_DIR}/${_customName})
+        endif()
+    else()
+        set(_target ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.xml)
+    endif()
+
+    add_custom_command(OUTPUT ${_target}
+        COMMAND ${QT_DBUSCPP2XML_EXECUTABLE} ${_qt4_dbus_options} ${_in_file} -o ${_target}
+        DEPENDS ${_in_file} VERBATIM
     )
-  ELSE(_optionalClassName)
-    ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
-       COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -i ${_include} -l ${_parentClass} ${_infile}
-       DEPENDS ${_infile} VERBATIM
-     )
-  ENDIF(_optionalClassName)
+endmacro()
 
-  QT5_GENERATE_MOC(${_header} ${_moc})
-  SET_SOURCE_FILES_PROPERTIES(${_impl} PROPERTIES SKIP_AUTOMOC TRUE)
-  MACRO_ADD_FILE_DEPENDENCIES(${_impl} ${_moc})
 
-  SET(${_sources} ${${_sources}} ${_impl} ${_header} ${_moc})
-ENDMACRO(QT5_ADD_DBUS_ADAPTOR)
+macro(QT5_ADD_DBUS_ADAPTOR _sources _xml_file _include _parentClass) # _optionalBasename _optionalClassName)
+    get_filename_component(_infile ${_xml_file} ABSOLUTE)
+
+    set(_optionalBasename "${ARGV4}")
+    if(_optionalBasename)
+        set(_basename ${_optionalBasename} )
+    else()
+        string(REGEX REPLACE "(.*[/\\.])?([^\\.]+)\\.xml" "\\2adaptor" _basename ${_infile})
+        string(TOLOWER ${_basename} _basename)
+    endif()
+
+    set(_optionalClassName "${ARGV5}")
+    set(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
+    set(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+    set(_moc    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
+
+    if(_optionalClassName)
+        add_custom_command(OUTPUT ${_impl} ${_header}
+          COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -c ${_optionalClassName} -i ${_include} -l ${_parentClass} ${_infile}
+          DEPENDS ${_infile} VERBATIM
+        )
+    else()
+        add_custom_command(OUTPUT ${_impl} ${_header}
+          COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} -m -a ${_basename} -i ${_include} -l ${_parentClass} ${_infile}
+          DEPENDS ${_infile} VERBATIM
+        )
+    endif()
+
+    qt5_generate_moc(${_header} ${_moc})
+    set_source_files_properties(${_impl} PROPERTIES SKIP_AUTOMOC TRUE)
+    macro_add_file_dependencies(${_impl} ${_moc})
+
+    set(${_sources} ${${_sources}} ${_impl} ${_header} ${_moc})
+endmacro()
