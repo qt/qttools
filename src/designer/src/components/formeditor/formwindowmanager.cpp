34102da56d4901f5e15e05cb47f76710a96babe9
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2012-02-13 15:24:42 +0100

    Remove AccessibilityPrepare.
    This enum value was never implemented in Qt in any meaningful way.
    It will be removed from qtbase.
    
    Change-Id: I29a89eeed5d232a1f4d2c835fd9fedb5d7e6fe97
    Reviewed-by: Jan-Arve Sæther <jan-arve.saether@nokia.com>

--- src/designer/src/components/formeditor/formwindowmanager.cpp
+++ src/designer/src/components/formeditor/formwindowmanager.cpp
@@ -162,7 +162,6 @@ bool FormWindowManager::eventFilter(QObject *o, QEvent *e)
     switch (eventType) { // Uninteresting events
     case QEvent::Create:
     case QEvent::Destroy:
-    case QEvent::AccessibilityPrepare:
     case QEvent::ActionAdded:
     case QEvent::ActionChanged:
     case QEvent::ActionRemoved:
