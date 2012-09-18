74d548c89dae83b392953f505876d8d383a72ce5
Oswald Buddenhagen <oswald.buddenhagen@digia.com>
2012-09-18 10:54:52 +0200

    fold m_qmakespecFull back into m_qmakespec
    the original value is not used any more after the final resolution.
    
    Change-Id: Icadc219f045a1bbfd20506c4c72c53d1fb352969
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    (cherry picked from qtbase/cc2cd0db5d0c53605ab93f796765667ead1920e8)

--- src/linguist/shared/qmakeevaluator.h
+++ src/linguist/shared/qmakeevaluator.h
@@ -279,7 +279,6 @@ public:
     bool m_valuemapInited;
     bool m_hostBuild;
     QString m_qmakespec;
-    QString m_qmakespecFull;
     QString m_qmakespecName;
     QString m_superfile;
     QString m_conffile;
