qt/qttools

commit 8695c1753964eeb06b5d9306010b56be5fc151c0

ActiveQt Designer plugin: Load control string correctly.
Task-number: QTBUG-29234

Change-Id: I5762c75efc24bf5322ff767a9dbecfdfc02c3377
Reviewed-by: Jarek Kobus <jaroslaw.kobus@digia.com>

src/designer/src/plugins/activeqt/qaxwidgetpropertysheet.cpp
@@ -48,6 +48,7 @@
 #include <QtDesigner/QDesignerPropertyEditorInterface>
 
 #include <QtDesigner/QExtensionManager>
+#include <private/qdesigner_utils_p.h>
 #include <QtCore/QDebug>
 #include <QtCore/QTimer>
 
@@ -111,7 +112,8 @@ void QAxWidgetPropertySheet::setProperty(int index, const QVariant &value)
     }
     // Loading forms: Reload
     if (name == m_controlProperty) {
-        const QString clsid = value.toString();
+        const qdesigner_internal::PropertySheetStringValue sv = qvariant_cast<qdesigner_internal::PropertySheetStringValue>(value);
+        const QString clsid = sv.value();
         if (clsid.isEmpty() || !axWidget()->loadControl(clsid))
             reset(index);
         else

commit 3e43b158ce158dc1285593d2c7555351033fc3ce

Active X plugin: Fix setting a control by task menu.
The property is a QString when set by the menu or a
PropertySheetStringValue when loading the form.

Breakage introduced by
8695c1753964eeb06b5d9306010b56be5fc151c0 .

Task-number: QTBUG-29234

Change-Id: I84c3de80b99c8a20aaf1f8f7289882b0092d6a0a
Reviewed-by: Andy Shaw <andy.shaw@digia.com>

src/designer/src/plugins/activeqt/qaxwidgetpropertysheet.cpp
@@ -112,8 +112,9 @@ void QAxWidgetPropertySheet::setProperty(int index, const QVariant &value)
     }
     // Loading forms: Reload
     if (name == m_controlProperty) {
-        const qdesigner_internal::PropertySheetStringValue sv = qvariant_cast<qdesigner_internal::PropertySheetStringValue>(value);
-        const QString clsid = sv.value();
+        const QString clsid = value.canConvert<qdesigner_internal::PropertySheetStringValue>() ?
+            qvariant_cast<qdesigner_internal::PropertySheetStringValue>(value).value() :
+            value.toString();
         if (clsid.isEmpty() || !axWidget()->loadControl(clsid))
             reset(index);
         else