qt/qttools

commit d73afeeafc7df8192508c1a5617944d303d5a0a2

Assistant: Fix static linking for Linux.
main.cpp:78: undefined reference to `qt_static_plugin_qsqlite()'

The import for the DB plugin should now be generated by qmake.

Change-Id: I9da25aea8667ca34221427236b8e1d36be77da8b
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>
(cherry picked from commit f212e46fe930ee89a8cb52beeeccc03369c015a1)
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/assistant/assistant/assistant.pro
@@ -107,13 +107,7 @@ mac {
     QMAKE_INFO_PLIST = Info_mac.plist
 }
 
-contains(QT_CONFIG, static): {
-    SQLPLUGINS = $$unique(sql-plugins)
-    contains(SQLPLUGINS, sqlite): {
-        QTPLUGIN += qsqlite
-        DEFINES += USE_STATIC_SQLITE_PLUGIN
-    }
-}
+QTPLUGIN += qsqlite
 
 target.path=$$[QT_INSTALL_BINS]
 INSTALLS += target

src/assistant/assistant/main.cpp
@@ -73,11 +73,6 @@
 
 QT_USE_NAMESPACE
 
-#if defined(USE_STATIC_SQLITE_PLUGIN)
-  #include <QtPlugin>
-  Q_IMPORT_PLUGIN(qsqlite)
-#endif
-
 namespace {
 
 void

commit 1b02287e464efc999dda10ff57c0207e6f3648d5

Assistant: Fix static linking on Windows.
Check for existence of sqlite-plugin.

Breakage introduced by d73afeeafc7df8192508c1a5617944d303d5a0a2 .

Change-Id: I2595ed4d80e0804f1c2503cddf94ca15e4922af9
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>

src/assistant/assistant/assistant.pro
@@ -107,7 +107,7 @@ mac {
     QMAKE_INFO_PLIST = Info_mac.plist
 }
 
-QTPLUGIN += qsqlite
+contains(SQLPLUGINS, sqlite):QTPLUGIN += qsqlite
 
 target.path=$$[QT_INSTALL_BINS]
 INSTALLS += target