c28ecbb7ed8404b7e29b1d1b30882c865d87e34d
Oswald Buddenhagen <oswald.buddenhagen@digia.com>
2013-09-05 18:30:05 +0200

    make setting a bad $$[QMAKEFEATURES] not crash qmake
    Task-number: QTBUG-29642
    Change-Id: I9cc209eb313f03bf342bcb64b1de3005755700a7
    (cherry picked from qtbase/50a90720be7d67dcdfe704a82fb3b1138edd2aab)
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    Reviewed-by: Daniel Teske <daniel.teske@digia.com>

--- src/linguist/shared/qmakeglobals.h
+++ src/linguist/shared/qmakeglobals.h
@@ -148,6 +148,7 @@ public:
 
     QString expandEnvVars(const QString &str) const;
     QString shadowedPath(const QString &fileName) const;
+    QStringList splitPathList(const QString &value) const;
 
 private:
     QString getEnv(const QString &) const;
