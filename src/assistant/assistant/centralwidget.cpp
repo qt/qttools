qt/qttools

commit 1ebfb744fedf0f9168290da05932079adcc2c536

Fix no such signal error on assistant startup with no webkit.
QObject::connect: No such signal HelpViewer::printRequested() in
qttools/src/assistant/assistant/centralwidget.cpp:626

Change-Id: If7dee3b8d38f659ca037b9d4ed73e01779f07c67
Reviewed-by: Karsten Heimrich <karsten.heimrich@digia.com>

src/assistant/assistant/centralwidget.cpp
@@ -615,6 +615,7 @@ void CentralWidget::initPrinter()
 void CentralWidget::connectSignals(HelpViewer *page)
 {
     TRACE_OBJ
+#if !defined(QT_NO_WEBKIT)
     connect(page, SIGNAL(copyAvailable(bool)), this,
         SIGNAL(copyAvailable(bool)));
     connect(page, SIGNAL(forwardAvailable(bool)), this,
@@ -625,6 +626,9 @@ void CentralWidget::connectSignals(HelpViewer *page)
         SLOT(handleSourceChanged(QUrl)));
     connect(page, SIGNAL(printRequested()), this, SLOT(print()));
     connect(page, SIGNAL(highlighted(QString)), this, SLOT(slotHighlighted(QString)));
+#else
+    Q_UNUSED(page)
+#endif
 }
 
 bool CentralWidget::eventFilter(QObject *object, QEvent *e)

commit 596a088b27447de3e67eece0fb5301119d9f0326

Fix broken navigation in assistant with no webkit.
Broken by 1ebfb744fedf0f9168290da05932079adcc2c536 . Only
printRequested() should be excluded.

Change-Id: I3c94aa62c469fc0cf32b7cbc8740aee3aeead6c2
Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>

src/assistant/assistant/centralwidget.cpp
@@ -616,6 +616,8 @@ void CentralWidget::connectSignals(HelpViewer *page)
 {
     TRACE_OBJ
 #if !defined(QT_NO_WEBKIT)
+    connect(page, SIGNAL(printRequested()), this, SLOT(print()));
+#endif
     connect(page, SIGNAL(copyAvailable(bool)), this,
         SIGNAL(copyAvailable(bool)));
     connect(page, SIGNAL(forwardAvailable(bool)), this,
@@ -624,11 +626,7 @@ void CentralWidget::connectSignals(HelpViewer *page)
         SIGNAL(backwardAvailable(bool)));
     connect(page, SIGNAL(sourceChanged(QUrl)), this,
         SLOT(handleSourceChanged(QUrl)));
-    connect(page, SIGNAL(printRequested()), this, SLOT(print()));
     connect(page, SIGNAL(highlighted(QString)), this, SLOT(slotHighlighted(QString)));
-#else
-    Q_UNUSED(page)
-#endif
 }
 
 bool CentralWidget::eventFilter(QObject *object, QEvent *e)