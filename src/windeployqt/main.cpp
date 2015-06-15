qt/qttools

commit 48b5cb2e682fd58e60b5650366c0291020e8b264

windeployqt: Add support for x86 based WinCE
Adapt windeployqt to support x86 based WinCE systems
in parallel to arm based WinCE systems.

Change-Id: I855db447524ebc55c68af5eaecbf9f6a448d9ed8
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
Reviewed-by: Andreas Holzammer <andreas.holzammer@kdab.com>
Reviewed-by: Björn Breitmeyer <bjoern.breitmeyer@kdab.com>

src/windeployqt/main.cpp
@@ -196,8 +196,12 @@ static Platform platformFromMkSpec(const QString &xSpec)
         return WinPhoneIntel;
     if (xSpec.startsWith(QLatin1String("winphone-arm")))
         return WinPhoneArm;
-    if (xSpec.startsWith(QLatin1String("wince")))
-        return WinCE;
+    if (xSpec.startsWith(QLatin1String("wince"))) {
+        if (xSpec.contains(QLatin1String("-x86-")))
+            return WinCEIntel;
+        if (xSpec.contains(QLatin1String("-arm")))
+            return WinCEArm;
+    }
     return UnknownPlatform;
 }
 
@@ -789,7 +793,8 @@ QStringList findQtPlugins(quint64 *usedQtModules, quint64 disabledQtModules,
                 switch (platform) {
                 case Windows:
                 case WindowsMinGW:
-                case WinCE:
+                case WinCEIntel:
+                case WinCEArm:
                     filter = QStringLiteral("qwindows");
                     break;
                 case WinRtIntel:
@@ -1179,13 +1184,13 @@ static DeployResult deploy(const Options &options,
     }
 
     // Check for ANGLE on the Qt5Gui library.
-    if ((options.platform & WindowsBased) && !qtGuiLibrary.isEmpty())  {
+    if ((options.platform & WindowsBased) && !(options.platform & WinCEIntel) && !(options.platform & WinCEArm) && !qtGuiLibrary.isEmpty())  {
         QString libGlesName = QStringLiteral("libGLESV2");
         if (isDebug)
             libGlesName += QLatin1Char('d');
         libGlesName += QLatin1String(windowsSharedLibrarySuffix);
         const QStringList guiLibraries = findDependentLibraries(qtGuiLibrary, options.platform, errorMessage);
-        const bool dependsOnAngle = !guiLibraries.filter(libGlesName, Qt::CaseInsensitive).isEmpty() && !(options.platform & WinCE);
+        const bool dependsOnAngle = !guiLibraries.filter(libGlesName, Qt::CaseInsensitive).isEmpty();
         const bool dependsOnOpenGl = !guiLibraries.filter(QStringLiteral("opengl32"), Qt::CaseInsensitive).isEmpty();
         if (options.angleDetection != Options::AngleDetectionForceOff
             && (dependsOnAngle || !dependsOnOpenGl || options.angleDetection == Options::AngleDetectionForceOn)) {

src/windeployqt/utils.h
@@ -62,7 +62,8 @@ enum Platform {
     WinRtArm = WindowsBased + ArmBased + 2,
     WinPhoneIntel = WindowsBased + IntelBased + 3,
     WinPhoneArm = WindowsBased + ArmBased + 4,
-    WinCE = WindowsBased + ArmBased + 5,
+    WinCEIntel = WindowsBased + IntelBased + 5,
+    WinCEArm = WindowsBased + ArmBased + 6,
     Unix = UnixBased,
     UnknownPlatform
 };

commit 162334916bb87feaa6285f9916c87c541615da35

windeployqt: Fix broken deployment of ANGLE.
Commit 48b5cb2e682fd58e60b5650366c0291020e8b264 (windeployqt: Add support
for x86 based WinCE) broke the if condition as revealed by g++:

main.cpp: In function 'DeployResult deploy(const Options&, const QMap<QString, QString>&, QString*)':
main.cpp:1189:77: warning: 'and' of mutually exclusive equal-tests is always 0
     if ((options.platform & WindowsBased) && !(options.platform & WinCEIntel) && !(options.platform & WinCEArm) && !qtGuiLibrary.isEmpty())  {

Change-Id: I91e8d4b93b1060330bff883f5eaa4eacc27e474f
Reviewed-by: Tobias Koenig <tobias.koenig@kdab.com>
Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

src/windeployqt/main.cpp
@@ -1186,7 +1186,8 @@ static DeployResult deploy(const Options &options,
     }
 
     // Check for ANGLE on the Qt5Gui library.
-    if ((options.platform & WindowsBased) && !(options.platform & WinCEIntel) && !(options.platform & WinCEArm) && !qtGuiLibrary.isEmpty())  {
+    if ((options.platform & WindowsBased) && options.platform != WinCEIntel
+        && options.platform != WinCEArm && !qtGuiLibrary.isEmpty())  {
         QString libGlesName = QStringLiteral("libGLESV2");
         if (isDebug)
             libGlesName += QLatin1Char('d');