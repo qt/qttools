1c4ee969c20e4285754f81de13f54f1d0cc2b82c
Tobias Koenig <tobias.koenig@kdab.com>
2015-12-09 09:57:11 +0100

    Haiku: Make 3rdparty clucene library compile
    Include the Haiku OS in the list of supported Unix systems
    
    Change-Id: I3096074c25f42595a47dde4457377fdf6857d3b3
    Reviewed-by: Augustin Cavalier <waddlesplash@gmail.com>
    Reviewed-by: Karsten Heimrich <karsten.heimrich@theqtcompany.com>

--- src/assistant/3rdparty/clucene/src/CLucene/config/compiler.h
+++ src/assistant/3rdparty/clucene/src/CLucene/config/compiler.h
@@ -65,7 +65,7 @@
 ////////////////////////////////////////////////////////////////////
 
 /* Operating system recognition (basic) */
-#if defined (__unix) || defined (__linux__) || defined (__QNX__) || defined (_AIX) || defined (__NetBSD__) || defined (__OpenBSD__) || defined (__Lynx__) || defined(hpux) || defined(__hpux)
+#if defined (__unix) || defined (__linux__) || defined (__QNX__) || defined (_AIX) || defined (__NetBSD__) || defined (__OpenBSD__) || defined (__Lynx__) || defined(hpux) || defined(__hpux) || defined(__HAIKU__)
 #undef  _UNIX
 #define _UNIX 1
 #include "CLucene/config/PlatformUnix.h"
