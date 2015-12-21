fe4c8a36bccf83b0026ef000af69cbc41b269386
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-12-21 14:11:10 +0100

    windeployqt: Fix version detection of Qt Quick Controls.
    The old code was based on the assumption that the Qt Labs Controls
    would have import paths like QtQuick/Controls.2, but that did not
    turn out. Check on hardcoded names instead.
    
    Change-Id: I7135f3e0c612663ca1be016ab949893f806adcbc
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

--- src/windeployqt/main.cpp
+++ src/windeployqt/main.cpp
@@ -1111,21 +1111,18 @@ static bool updateLibrary(const QString &sourceFileName, const QString &targetDi
 // 'QtQuick/Controls' ==> 1, or 'QtQuick/Controls.2' ==> 2.
 static inline int quickControlsImportPath(const QString &ip)
 {
-    if (ip.endsWith(QLatin1String("QtQuick/Dialogs")) || ip.contains(QLatin1String("QtQuick/Dialogs/")))
-        return 1; // Dialogs only in v1, so far.
-    const QString controlsPattern = QStringLiteral("QtQuick/Controls");
-    const int pos = ip.indexOf(controlsPattern);
-    if (pos < 0)
-        return 0;
-    const int endPos = pos + controlsPattern.size();
-    if (endPos == ip.size() || ip.at(endPos) == QLatin1Char('/'))
-        return 1; // No version: v1
-    if (ip.at(endPos) != QLatin1Char('.'))
-        return 0;
-    const int versionPos = endPos + 1;
-    int versionEndPos = versionPos;
-    for ( ; versionEndPos < ip.size() && ip.at(versionEndPos).isDigit(); ++versionEndPos) {}
-    return ip.midRef(versionPos, versionEndPos - versionPos).toInt();
+    if (ip.endsWith(QLatin1String("QtQuick/Dialogs")) || ip.contains(QLatin1String("QtQuick/Dialogs/"))
+        || ip.contains(QLatin1String("QtQuick/Controls"))) {
+        return 1; // Dialogs only in v1, so far; no version number on directory: v1.
+    }
+    if (ip.contains(QLatin1String("Qt/labs/calendar"))
+        || ip.contains(QLatin1String("Qt/labs/controls"))
+        || ip.contains(QLatin1String("Qt/labs/folderlistmodel"))
+        || ip.contains(QLatin1String("Qt/labs/settings"))
+        || ip.contains(QLatin1String("Qt/labs/templates"))) {
+        return 2;
+    }
+    return 0; // Non-controls import
 }
 
 static DeployResult deploy(const Options &options,
