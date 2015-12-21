b8c88173de60e3962fddea2a53aad0f7d0e1b630
Kevin Funk <kevin.funk@kdab.com>
2015-12-21 14:59:10 +0100

    Split up test_translation_macros
    Split up into two individual tests:
    * test_add_translation_macro
    * test_create_translation_macro
    
    Required for upcoming extension of qt5_add_translation macro
    
    Change-Id: Iaf18aed493119bdbefec3790a7a29bb42c6925b7
    Reviewed-by: Stephen Kelly <steveire@gmail.com>

--- tests/auto/cmake/test_add_translation_macro/myobject_de.ts
+++ tests/auto/cmake/test_add_translation_macro/myobject_de.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="de_DE">
+<TS version="2.1" language="de_DE">
 <context>
     <name>QObject</name>
     <message>
-        <location filename="myi18nobject.cpp" line="15"/>
+        <location filename="myi18nobject.cpp" line="49"/>
         <source>Hello, world!</source>
-        <translation>Hallo, Welt</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
