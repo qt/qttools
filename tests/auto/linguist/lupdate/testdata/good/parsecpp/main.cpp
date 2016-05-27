qt/qttools

commit 4ea581c95ae9704b250591c5d4d9b0517f5b62f2

lupdate: Explicitly handle C++ member initializer lists
This change makes the cpp parser explicitly deal with parens and
braces in member initializer lists of constructors. It fixes
problems with C++11 syntax like list-initialization and lambdas.
It adds auto tests for tr() calls in initialization expressions.

The combination of parens in base class template parameters with
list initialization (braces) remains unsupported.

Task-number: QTBUG-34128
Task-number: QTBUG-42166
Change-Id: Id4ac0a011fcda8d2b72a15348a1b96fdd492ab32
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/linguist/lupdate/cpp.cpp
@@ -1757,6 +1757,7 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
     QString functionName;
 #endif
     bool yyTokColonSeen = false; // Start of c'tor's initializer list
+    bool yyTokIdentSeen = false; // Start of initializer (member or base class)
     metaExpected = true;
 
     prospectiveContext.clear();
@@ -1978,6 +1979,11 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
             yyTok = getToken();
             break;
         case Tok_Ident:
+            if (yyTokColonSeen &&
+                yyBraceDepth == namespaceDepths.count() && yyParenDepth == 0) {
+                // member or base class identifier
+                yyTokIdentSeen = true;
+            }
             yyTok = getToken();
             if (yyTok == Tok_LeftParen) {
                 switch (trFunctionAliasManager.trFunctionByName(yyWord)) {
@@ -2034,6 +2040,11 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
             }
             break;
         case Tok_ColonColon:
+            if (yyTokIdentSeen) {
+                // member or base class identifier
+                yyTok = getToken();
+                break;
+            }
             if (yyBraceDepth == namespaceDepths.count() && yyParenDepth == 0 && !yyTokColonSeen)
                 prospectiveContext = prefix;
             prefix += strColons;
@@ -2046,16 +2057,20 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
 #endif
             break;
         case Tok_RightBrace:
-            if (yyBraceDepth + 1 == namespaceDepths.count()) // class or namespace
-                truncateNamespaces(&namespaces, namespaceDepths.pop());
-            if (yyBraceDepth == namespaceDepths.count()) {
-                // function, class or namespace
-                if (!yyBraceDepth && !directInclude)
-                    truncateNamespaces(&functionContext, 1);
-                else
-                    functionContext = namespaces;
-                functionContextUnresolved.clear();
-                pendingContext.clear();
+            if (!yyTokColonSeen) {
+                if (yyBraceDepth + 1 == namespaceDepths.count()) {
+                    // class or namespace
+                    truncateNamespaces(&namespaces, namespaceDepths.pop());
+                }
+                if (yyBraceDepth == namespaceDepths.count()) {
+                    // function, class or namespace
+                    if (!yyBraceDepth && !directInclude)
+                        truncateNamespaces(&functionContext, 1);
+                    else
+                        functionContext = namespaces;
+                    functionContextUnresolved.clear();
+                    pendingContext.clear();
+                }
             }
             // fallthrough
         case Tok_Semicolon:
@@ -2094,15 +2109,21 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
             yyTok = getToken();
             break;
         case Tok_LeftBrace:
-            if (!prospectiveContext.isEmpty()
-                && yyBraceDepth == namespaceDepths.count() + 1 && yyParenDepth == 0) {
-                pendingContext = prospectiveContext;
-                prospectiveContext.clear();
+            if (yyBraceDepth == namespaceDepths.count() + 1 && yyParenDepth == 0) {
+                if (!prospectiveContext.isEmpty()) {
+                    pendingContext = prospectiveContext;
+                    prospectiveContext.clear();
+                }
+                if (!yyTokIdentSeen) {
+                    // Function body
+                    yyTokColonSeen = false;
+                }
             }
-            yyTokColonSeen = false;
+            // fallthrough
+        case Tok_LeftParen:
+            yyTokIdentSeen = false;
             // fallthrough
         case Tok_Comma:
-        case Tok_LeftParen:
         case Tok_QuestionMark:
             metaExpected = true;
             yyTok = getToken();

tests/auto/linguist/lupdate/testdata/good/parsecpp/main.cpp
@@ -516,3 +516,114 @@ void nullMacroInPlural()
     QObject::tr("%n NULL(s)", NULL, 3);
     QObject::tr("%n Q_NULLPTR(s)", Q_NULLPTR, 3);
 }
+
+
+
+// QTBUG-34128: lupdate ignores tr() calls in constructor if a member is
+// initialized with C++11 initializer list
+class ListInitializationClass : public NameSchpase::YetMoreFun, Gui::BaseClass
+{
+    Q_OBJECT
+
+    ListInitializationClass() :
+        NameSchpase::YetMoreFun(),
+        Gui::BaseClass{ },
+        a{ 0 },
+        b(1),
+        c(tr("Hello World"))
+    {
+        tr("ListInitializationClass in-class constructor");
+    }
+
+    ListInitializationClass(int a);
+
+    ListInitializationClass(int a, int b, int c);
+
+    int a;
+    int b;
+    QString c;
+};
+
+ListInitializationClass::ListInitializationClass(int a) :
+    b{ { 2, 3 }[a] }
+{
+    tr("ListInitializationClass out-of-class single member initializer");
+}
+
+ListInitializationClass::ListInitializationClass(int a, int b, int c) :
+    NameSchpase::YetMoreFun{ },
+    Gui::BaseClass(),
+    a{ 2 + (a/3) },
+    b(b),
+    c{ tr("%n item(s)", Q_NULLPTR, c) }
+{
+    tr("ListInitializationClass out-of-class multi member initializer");
+}
+
+
+
+// QTBUG-42166: lupdate is confused by C++11 lambdas in constructor initializer lists
+class LambdaMemberClass : public Gui::BaseClass
+{
+    Q_OBJECT
+
+    LambdaMemberClass() :
+        Gui::BaseClass(),
+        a{ [](){ std::cout << QObject::tr("Hello"); } },
+        b([](){ std::cout << "World\n"; })
+    {
+        tr("LambdaMemberClass in-class constructor");
+    }
+
+    LambdaMemberClass(void *);
+
+    std::function<void()> a;
+    std::function<void()> b;
+};
+
+LambdaMemberClass::LambdaMemberClass(void *) :
+    Gui::BaseClass{ },
+    a([](){ std::cout << QObject::tr("Hallo "); }),
+    b{ [](){ std::cout << "Welt\n"; } }
+{
+    tr("LambdaMemberClass out-of-class constructor");
+}
+
+
+
+// Template parameters in base class initialization
+class TemplateClass : QVarLengthArray<char, sizeof(std::size_t)>, std::vector<int>
+{
+    Q_DECLARE_TR_FUNCTIONS(TemplateClass)
+    QString member;
+
+public:
+    TemplateClass() :
+        QVarLengthArray<char, sizeof(std::size_t)>(),
+        std::vector<int>(3),
+        member(tr("TemplateClass() in-class member initialization"))
+    {
+        tr("TemplateClass() in-class body");
+    }
+
+    TemplateClass(void *);
+    TemplateClass(int);
+};
+
+// supported: combination of parens in base class template parameter with direct initialization (parens)
+TemplateClass::TemplateClass(void *) :
+    QVarLengthArray<char, sizeof(std::size_t)>(),
+    std::vector<int>{ 1, 2 },
+    member{ tr("TemplateClass(void *) out-of-class member initialization") }
+{
+    tr("TemplateClass(void *) out-of-class body");
+}
+
+// not supported: combination of parens in base class template parameter with list initialization (braces)
+TemplateClass::TemplateClass(int) :
+    QVarLengthArray<char, sizeof(std::size_t)>{ 3, 4, 5 },
+    member(tr("[unsupported] TemplateClass(int) out-of-class member initialization"))
+{
+    tr("[unsupported] TemplateClass(int) out-of-class body");
+}
+

tests/auto/linguist/lupdate/testdata/good/parsecpp/project.ts.result
@@ -187,6 +187,49 @@ backslashed \ stuff.</source>
     </message>
 </context>
 <context>
+    <name>LambdaMemberClass</name>
+    <message>
+        <location filename="main.cpp" line="575"/>
+        <source>LambdaMemberClass in-class constructor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="589"/>
+        <source>LambdaMemberClass out-of-class constructor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ListInitializationClass</name>
+    <message>
+        <location filename="main.cpp" line="533"/>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="535"/>
+        <source>ListInitializationClass in-class constructor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="550"/>
+        <source>ListInitializationClass out-of-class single member initializer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="main.cpp" line="558"/>
+        <source>%n item(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="560"/>
+        <source>ListInitializationClass out-of-class multi member initializer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>LotsaFun</name>
     <message>
         <location filename="main.cpp" line="310"/>
@@ -334,6 +377,16 @@ backslashed \ stuff.</source>
         </translation>
     </message>
     <message>
+        <location filename="main.cpp" line="572"/>
+        <source>Hello</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="586"/>
+        <source>Hallo </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="included.cpp" line="34"/>
         <source>message from #included .cpp file</source>
         <translation type="unfinished"></translation>
@@ -383,6 +436,29 @@ backslashed \ stuff.</source>
         <translation type="unfinished">
             <numerusform></numerusform>
         </translation>
+    </message>
+</context>
+<context>
+    <name>TemplateClass</name>
+    <message>
+        <location filename="main.cpp" line="604"/>
+        <source>TemplateClass() in-class member initialization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="606"/>
+        <source>TemplateClass() in-class body</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="617"/>
+        <source>TemplateClass(void *) out-of-class member initialization</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="619"/>
+        <source>TemplateClass(void *) out-of-class body</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>

tests/auto/linguist/lupdate/testdata/good/parsecpp/expectedoutput.txt
@@ -2,4 +2,6 @@
 .*/lupdate/testdata/good/parsecpp/finddialog.cpp:127: Qualifying with unknown namespace/class ::FindDialog
 .*/lupdate/testdata/good/parsecpp/finddialog.cpp:167: Qualifying with unknown namespace/class ::FindDialog
 .*/lupdate/testdata/good/parsecpp/finddialog.cpp:173: Unsupported encoding Latin1
+.*/lupdate/testdata/good/parsecpp/main.cpp:625: tr\(\) cannot be called without context
+.*/lupdate/testdata/good/parsecpp/main.cpp:627: tr\(\) cannot be called without context
 lupdate warning: Message with id 'yet_another_id' has no source.

commit abcb49adcc640817b37d4ba8f0c10712a857d434

lupdate: Fix parsing of class template specializations
4ea581c95ae9704b250591c5d4d9b0517f5b62f2 introduced a bug where
class template specialization would lead to contexts not being properly
cleaned up on '}'. The occurrence of class template specializations
lead to a massive slowdown in parsing following includes because they
were treated as direct (in-namespace) and parsed more often than
normally needed.

Unfortunately the bug was not discovered early by the auto tests,
but eventually the slowdown appeared in CI when merging to dev.
This change adds a test for class template specializations. It would
discover the cause (broken context) which triggers the slowdown but
doesn't depend on includes.

Task-number: QTBUG-53644
Change-Id: I94cce58702b53dfb5e0f9c92585222dd3f938593
Reviewed-by: Liang Qi <liang.qi@qt.io>
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/linguist/lupdate/cpp.cpp
@@ -1848,16 +1848,16 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
                     }
                 }
 
-                if (yyTok == Tok_Colon) {
-                    // Skip any token until '{' since we might do things wrong if we find
-                    // a '::' token here.
+                if (yyTok == Tok_Colon || yyTok == Tok_Other) {
+                    // Skip any token until '{' or ';' since we might do things wrong if we find
+                    // a '::' or ':' token here.
                     do {
                         yyTok = getToken();
                         if (yyTok == Tok_Eof)
                             goto goteof;
                         if (yyTok == Tok_Cancel)
                             goto case_default;
-                    } while (yyTok != Tok_LeftBrace);
+                    } while (yyTok != Tok_LeftBrace && yyTok != Tok_Semicolon);
                 } else {
                     if (yyTok != Tok_LeftBrace) {
                         // Obviously a forward declaration. We skip those, as they

tests/auto/linguist/lupdate/testdata/good/parsecpp/main.cpp
@@ -627,3 +627,20 @@ TemplateClass::TemplateClass(int) :
     tr("[unsupported] TemplateClass(int) out-of-class body");
 }
 
+
+
+// Related to QTBUG-53644, adapted from qglobal.h.
+// Namespace Private must be parsed correctly for TranslatedAfterPrivate to work.
+namespace Private {
+    template <class T> struct Class1 { T t; };
+    template <class T> struct Class1<T &> : Class1<T> {};
+    template <class T> struct Class2 { enum { Value = sizeof(T) }; };
+}  // namespace Private
+class TranslatedAfterPrivate
+{
+    Q_OBJECT
+    TranslatedAfterPrivate()
+    {
+        tr("Must be in context TranslatedAfterPrivate");
+    }
+};

tests/auto/linguist/lupdate/testdata/good/parsecpp/project.ts.result
@@ -555,6 +555,14 @@ backslashed \ stuff.</source>
     </message>
 </context>
 <context>
+    <name>TranslatedAfterPrivate</name>
+    <message>
+        <location filename="main.cpp" line="644"/>
+        <source>Must be in context TranslatedAfterPrivate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>YetAnotherTest</name>
     <message>
         <location filename="main.cpp" line="239"/>