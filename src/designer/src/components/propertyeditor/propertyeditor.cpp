qt/qttools

commit 548a75049efaa3f9b3087016e3e36cc9b4f23e2e

Qt Designer: Fix crash when morphing widgets with buddies
Bail out of PropertyEditor::setObject() when no form window
can be found for the current widget, which can happen when
setting the buddy property in morph macro sequences.

Task-number: QTBUG-68507
Change-Id: I61955121d57e1c18e2ccf93d63e51d2df24f0827
Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

src/designer/src/components/propertyeditor/propertyeditor.cpp
@@ -911,6 +911,8 @@ void PropertyEditor::setObject(QObject *object)
     m_object = object;
     m_propertyManager->setObject(object);
     QDesignerFormWindowInterface *formWindow = QDesignerFormWindowInterface::findFormWindow(m_object);
+    if (Q_UNLIKELY(formWindow == nullptr)) // QTBUG-68507, can happen in Morph Undo macros with buddies
+        return;
     FormWindowBase *fwb = qobject_cast<FormWindowBase *>(formWindow);
     const bool idIdBasedTranslation = fwb && fwb->useIdBasedTranslations();
     const bool idIdBasedTranslationUnchanged = (idIdBasedTranslation == DesignerPropertyManager::useIdBasedTranslations());

commit 63f128103f666e72f5f547871da44e49a9cbafaf

Qt Designer: Fix crash when reloading forms with resources
Change qtools/548a75049efaa3f9b3087016e3e36cc9b4f23e2e caused
icon/pixmap properties to leak when setting a null object on the
property editor since the code removing the properties was not
executed.

Default to the active form window when trying to find a form window
for the object in question and bail out with a warning if that fails.

Fixes: QTBUG-71809
Task-number: QTBUG-68507
Change-Id: I3fd96dd9b72d822c47071286b641254530f1568d
Reviewed-by: Kai Koehne <kai.koehne@qt.io>

src/designer/src/components/propertyeditor/propertyeditor.cpp
@@ -913,8 +913,15 @@ void PropertyEditor::setObject(QObject *object)
     m_object = object;
     m_propertyManager->setObject(object);
     QDesignerFormWindowInterface *formWindow = QDesignerFormWindowInterface::findFormWindow(m_object);
-    if (Q_UNLIKELY(formWindow == nullptr)) // QTBUG-68507, can happen in Morph Undo macros with buddies
-        return;
+    // QTBUG-68507: Form window can be null for objects in Morph Undo macros with buddies
+    if (object != nullptr && formWindow == nullptr) {
+        formWindow = m_core->formWindowManager()->activeFormWindow();
+        if (formWindow == nullptr) {
+            qWarning("PropertyEditor::setObject(): Unable to find form window for \"%s\".",
+                     qPrintable(object->objectName()));
+            return;
+        }
+    }
     FormWindowBase *fwb = qobject_cast<FormWindowBase *>(formWindow);
     const bool idIdBasedTranslation = fwb && fwb->useIdBasedTranslations();
     const bool idIdBasedTranslationUnchanged = (idIdBasedTranslation == DesignerPropertyManager::useIdBasedTranslations());