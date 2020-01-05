4970167b75fb83bd46227d9d9315b6e215c8e457
Paul Wicking <paul.wicking@qt.io>
2020-01-05 13:04:24 +0100

    QDoc: Drop xml prolog from html output
    The html output is not well-formed xml, so let's not claim that it is.
    
    Update expected output in tests to reflect the change in output.
    
    Task-number: QTBUG-73026
    Change-Id: I0e6646eb33cafad7be543869585609d6f4cb0fdb
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/qdoc/htmlgenerator.cpp
+++ src/qdoc/htmlgenerator.cpp
@@ -1830,11 +1830,6 @@ void HtmlGenerator::generateNavigationBar(const QString &title, const Node *node
 
 void HtmlGenerator::generateHeader(const QString &title, const Node *node, CodeMarker *marker)
 {
-#ifndef QT_NO_TEXTCODEC
-    out() << QString("<?xml version=\"1.0\" encoding=\"%1\"?>\n").arg(outputEncoding);
-#else
-    out() << QString("<?xml version=\"1.0\"?>\n");
-#endif
     out() << "<!DOCTYPE html>\n";
     out() << QString("<html lang=\"%1\">\n").arg(naturalLanguage);
     out() << "<head>\n";
