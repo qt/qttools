f3180b6eb650598ca937943d6cd66da13335d9de
Morten Johan Sørvig <morten.sorvig@qt.io>
2020-09-22 19:12:52 +0200

    Revert "macdeployqt: Don't copy .prl files into the Resources folder"
    This was skipping other files from Resources/ as well,
    such as “en-GB.pak” and “QtWebEngineProcess”.
    
    This reverts commit aabba72f7965e06e2e6ed960d8cf8078249dac8c.
    
    Fixes: QTBUG-86759
    Change-Id: I6d27e3a32b479e1b6765783af71dd16826b181ca
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@qt.io>
    (cherry picked from commit 2727412238300d847493351e706f01d180c64d70)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/macdeployqt/shared/shared.cpp
+++ src/macdeployqt/shared/shared.cpp
@@ -640,8 +640,7 @@ QStringList getBinaryDependencies(const QString executablePath,
 }
 
 // copies everything _inside_ sourcePath to destinationPath
-bool recursiveCopy(const QString &sourcePath, const QString &destinationPath,
-                   const QRegularExpression &ignoreRegExp = QRegularExpression())
+bool recursiveCopy(const QString &sourcePath, const QString &destinationPath)
 {
     if (!QDir(sourcePath).exists())
         return false;
@@ -650,10 +649,7 @@ bool recursiveCopy(const QString &sourcePath, const QString &destinationPath,
     LogNormal() << "copy:" << sourcePath << destinationPath;
 
     QStringList files = QDir(sourcePath).entryList(QStringList() << "*", QDir::Files | QDir::NoDotAndDotDot);
-    const bool hasValidRegExp = ignoreRegExp.isValid();
     foreach (QString file, files) {
-        if (hasValidRegExp && ignoreRegExp.match(file).hasMatch())
-            continue;
         const QString fileSourcePath = sourcePath + "/" + file;
         const QString fileDestinationPath = destinationPath + "/" + file;
         copyFilePrintStatus(fileSourcePath, fileDestinationPath);
@@ -786,9 +782,7 @@ QString copyFramework(const FrameworkInfo &framework, const QString path)
     // Copy Resouces/, Libraries/ and Helpers/
     const QString resourcesSourcePath = framework.frameworkPath + "/Resources";
     const QString resourcesDestianationPath = frameworkDestinationDirectory + "/Versions/" + framework.version + "/Resources";
-    // Ignore *.prl files that are in the Resources directory
-    recursiveCopy(resourcesSourcePath, resourcesDestianationPath,
-                  QRegularExpression("\\A(?:[^/]*\\.prl)\\z"));
+    recursiveCopy(resourcesSourcePath, resourcesDestianationPath);
     const QString librariesSourcePath = framework.frameworkPath + "/Libraries";
     const QString librariesDestianationPath = frameworkDestinationDirectory + "/Versions/" + framework.version + "/Libraries";
     bool createdLibraries = recursiveCopy(librariesSourcePath, librariesDestianationPath);
