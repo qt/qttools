15ebf3327191335c2c6eac4114ccb0553dc34f63
Topi Reinio <topi.reinio@qt.io>
2020-10-22 16:41:53 +0200

    qdoc: Fix broken links generated by \sincelist
    The combination of single-exec mode and outputting to subdirectories
    (offline mode) made \sincelist generate href's to new classes and
    functions that are missing the target subdirectory.
    
    This happened because we did not pass the relative node to the
    function(s) that generate that list - proper link resolution
    requires a check if the page containing the \sincelist command
    comes from a different doc module, and the relative node is
    used for that.
    
    Pick-to: 5.15
    Fixes: QTBUG-87802
    Change-Id: I30024d651a857b15375ef7e35dfaa21f0193ec46
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/qdoc/htmlgenerator.cpp
+++ src/qdoc/htmlgenerator.cpp
@@ -562,9 +562,9 @@ int HtmlGenerator::generateAtom(const Atom *atom, const Node *relative, CodeMark
                 out() << "<a name=\"" << Doc::canonicalTitle(section.title()) << "\"></a>\n";
                 out() << "<h3>" << protectEnc(section.title()) << "</h3>\n";
                 if (index == Sections::SinceClasses)
-                    generateCompactList(Generic, nullptr, ncmap, false, QStringLiteral("Q"));
+                    generateCompactList(Generic, relative, ncmap, false, QStringLiteral("Q"));
                 else if (index == Sections::SinceQmlTypes)
-                    generateCompactList(Generic, nullptr, nqcmap, false, QStringLiteral(""));
+                    generateCompactList(Generic, relative, nqcmap, false, QStringLiteral(""));
                 else if (index == Sections::SinceMemberFunctions) {
                     ParentMaps parentmaps;
                     ParentMaps::iterator pmap;
@@ -580,17 +580,17 @@ int HtmlGenerator::generateAtom(const Atom *atom, const Node *relative, CodeMark
                         NodeVector nv = map->values().toVector();
                         out() << "<p>Class ";
 
-                        out() << "<a href=\"" << linkForNode(map.key(), nullptr) << "\">";
+                        out() << "<a href=\"" << linkForNode(map.key(), relative) << "\">";
                         QStringList pieces = map.key()->fullName().split("::");
                         out() << protectEnc(pieces.last());
                         out() << "</a>"
                               << ":</p>\n";
 
-                        generateSection(nv, nullptr, marker);
+                        generateSection(nv, relative, marker);
                         out() << "<br/>";
                     }
                 } else {
-                    generateSection(section.members(), nullptr, marker);
+                    generateSection(section.members(), relative, marker);
                 }
             }
             ++index;
