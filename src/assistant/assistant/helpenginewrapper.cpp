qt/qttools

commit 15d7a30d41b423846fc74d9dd252c1e2a84769e2

QtHelp: Reverse the default state for readonly property for help engine
From now the read only property is true by default.
The old workaround with setting the dynamic property
'_q_readonly' to true for the help engine should still work,
so the change shouldn't break the user code with the workaround.

This fixes the most common usage of QtHelp, when help engine is used
for read only help collection files.

Task-number: QTBUG-83268
Task-number: QTBUG-82126
Task-number: QTBUG-84745
Change-Id: Iee6fd18269d68f43633a5fecbb65abc60c99091a
Reviewed-by: Kai Koehne <kai.koehne@qt.io>

src/assistant/assistant/helpenginewrapper.cpp
@@ -699,6 +699,7 @@ HelpEngineWrapperPrivate::HelpEngineWrapperPrivate(const QString &collectionFile
 {
     TRACE_OBJ
     initFileSystemWatchers();
+    m_helpEngine->setReadOnly(false);
     m_helpEngine->setUsesFilterEngine(true);
 }
 

src/assistant/assistant/main.cpp
@@ -274,7 +274,6 @@ static ExitStatus preliminarySetup(CmdLineParser *cmd)
     QScopedPointer<QHelpEngineCore> collection;
     if (collectionFileGiven) {
         collection.reset(new QHelpEngineCore(collectionFile));
-        collection->setProperty("_q_readonly", QVariant::fromValue<bool>(true));
         if (!collection->setupData()) {
             cmd->showMessage(QCoreApplication::translate("Assistant",
                              "Error reading collection file '%1': %2.")

src/assistant/help/qhelpcollectionhandler_p.h
@@ -257,7 +257,7 @@ private:
     QString m_connectionName;
     QSqlQuery *m_query = nullptr;
     bool m_vacuumScheduled = false;
-    bool m_readOnly = false;
+    bool m_readOnly = true;
 };
 
 QT_END_NAMESPACE

src/assistant/help/qhelpcontentwidget.cpp
@@ -276,7 +276,6 @@ void QHelpContentProvider::run()
         return;
 
     QHelpCollectionHandler collectionHandler(collectionFile);
-    collectionHandler.setReadOnly(true);
     if (!collectionHandler.openCollectionFile())
         return;
 

src/assistant/help/qhelpengine_p.h
@@ -89,6 +89,7 @@ public:
     bool needsSetup = true;
     bool autoSaveFilter = true;
     bool usesFilterEngine = false;
+    bool readOnly = true;
 
 protected:
     QHelpEngineCore *q;

src/assistant/help/qhelpenginecore.cpp
@@ -81,7 +81,7 @@ bool QHelpEngineCorePrivate::setup()
 
     const QVariant readOnlyVariant = q->property("_q_readonly");
     const bool readOnly = readOnlyVariant.isValid()
-            ? readOnlyVariant.toBool() : false;
+            ? readOnlyVariant.toBool() : q->isReadOnly();
     collectionHandler->setReadOnly(readOnly);
     const bool opened = collectionHandler->openCollectionFile();
     if (opened)
@@ -124,6 +124,18 @@ void QHelpEngineCorePrivate::errorReceived(const QString &msg)
     \note QHelpFilterEngine replaces the older filter API that is
     deprecated since Qt 5.13. Call setUsesFilterEngine() with \c true to
     enable the new functionality.
+
+    The core help engine has two modes:
+    \list
+        \li Read-only mode, where the help collection file is not changed
+            unless explicitly requested. This also works if the
+            collection file is in a read-only location,
+            and is the default.
+        \li Fully writable mode, which requires the help collection
+            file to be writable.
+    \endlist
+    The mode can be changed by calling setReadOnly() method, prior to
+    calling setupData().
 
     The help engine also offers the possibility to set and read values
     in a persistent way comparable to ini files or Windows registry
@@ -232,6 +244,31 @@ void QHelpEngineCore::setCollectionFile(const QString &fileName)
     }
     d->init(fileName, this);
     d->needsSetup = true;
+}
+
+/*!
+    \property QHelpEngineCore::readOnly
+    \brief whether the help engine is read-only.
+    \since 6.0
+
+    In read-only mode, the user can use the help engine
+    with a collection file installed in a read-only location.
+    In this case, some functionality won't be accessible,
+    like registering additional documentation, filter editing,
+    or any action that would require changes to the
+    collection file. Setting it to \c false enables the full
+    functionality of the help engine.
+
+    By default, this property is \c true.
+*/
+bool QHelpEngineCore::isReadOnly() const
+{
+    return d->readOnly;
+}
+
+void QHelpEngineCore::setReadOnly(bool enable)
+{
+    d->readOnly = enable;
 }
 
 /*!

src/assistant/help/qhelpenginecore.h
@@ -58,6 +58,7 @@ class QHELP_EXPORT QHelpEngineCore : public QObject
     Q_OBJECT
     Q_PROPERTY(bool autoSaveFilter READ autoSaveFilter WRITE setAutoSaveFilter)
     Q_PROPERTY(QString collectionFile READ collectionFile WRITE setCollectionFile)
+    Q_PROPERTY(bool readOnly READ isReadOnly WRITE setReadOnly)
 #if QT_DEPRECATED_SINCE(5, 15)
     Q_PROPERTY(QString currentFilter READ currentFilter WRITE setCurrentFilter)
 #endif
@@ -65,6 +66,9 @@ class QHELP_EXPORT QHelpEngineCore : public QObject
 public:
     explicit QHelpEngineCore(const QString &collectionFile, QObject *parent = nullptr);
     virtual ~QHelpEngineCore();
+
+    bool isReadOnly() const;
+    void setReadOnly(bool enable);
 
     QHelpFilterEngine *filterEngine() const;
 

src/assistant/help/qhelpindexwidget.cpp
@@ -135,7 +135,6 @@ void QHelpIndexProvider::run()
         return;
 
     QHelpCollectionHandler collectionHandler(collectionFile);
-    collectionHandler.setReadOnly(true);
     if (!collectionHandler.openCollectionFile())
         return;
 

commit 633505bc5490d174ac3985fb5e195b4e742b987b

Assistant: Fix missing documentation
Move a call to initFileSystemWatchers() after
setting the readonly mode, since initFileSystemWatchers()
triggers setup with unwanted mode.
Broken by accident in 15d7a30d41b423846fc74d9dd252c1e2a84769e2

Fixes: QTBUG-87783
Change-Id: I6d79fea010af28c2a839ef4aa6f0f802669ea7bd
Reviewed-by: hjk <hjk@qt.io>

src/assistant/assistant/helpenginewrapper.cpp
@@ -698,9 +698,9 @@ HelpEngineWrapperPrivate::HelpEngineWrapperPrivate(const QString &collectionFile
       m_qchWatcher(new QFileSystemWatcher(this))
 {
     TRACE_OBJ
-    initFileSystemWatchers();
     m_helpEngine->setReadOnly(false);
     m_helpEngine->setUsesFilterEngine(true);
+    initFileSystemWatchers();
 }
 
 void HelpEngineWrapperPrivate::initFileSystemWatchers()