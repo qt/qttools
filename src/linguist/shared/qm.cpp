qt/qttools

commit b96fe95da00aca6be4a30f7d6216f41e6538ec28

Fix check for malformed input when decoding translations
Use patch 458d49861f5 in qtbase also for the matching functionality in
qttools. Any integer modulo 1 can never be anything else but 0, so the
statement could never be true.

Found by static analyzer: https://www.viva64.com/en/amp/b/0801/

Pick-to: 5.15
Pick-to: 6.1
Change-Id: I92d13877e5e44bb7555be7fb89c1ee546b25fbba
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/linguist/shared/qm.cpp
@@ -546,7 +546,7 @@ bool loadQM(Translator &translator, QIODevice &dev, ConversionData &cd)
                 goto end;
             case Tag_Translation: {
                 int len = read32(m);
-                if (len % 1) {
+                if (len & 1) {
                     cd.appendError(QLatin1String("QM-Format error"));
                     return false;
                 }

commit b9a9f7b502f0631144176be343779a698e54161e

Fix regression in lconvert handling empty translations
Fixes a regression introduced by commit b96fe95da00aca6b, which enabled
a sanity check that now failed to account for empty string.

The serialized format of a QString is such that '-1' indicates a null
string, otherwise it's <length><QChar>+. Since QChar is 2 bytes,
length should therefore always be an even number ... except for -1.

While at it, also preserve the difference between an empty string and
a null string.

Fixes: QTBUG-91558
Pick-to: 6.1
Change-Id: Iffbc6ee2c94b8363d2c1d91440022a77dbef4772
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/linguist/shared/qm.cpp
@@ -546,12 +546,17 @@ bool loadQM(Translator &translator, QIODevice &dev, ConversionData &cd)
                 goto end;
             case Tag_Translation: {
                 int len = read32(m);
-                if (len & 1) {
+                m += 4;
+
+                // -1 indicates an empty string
+                // Otherwise streaming format is UTF-16 -> 2 bytes per character
+                if ((len != -1) && (len & 1)) {
                     cd.appendError(QLatin1String("QM-Format error"));
                     return false;
                 }
-                m += 4;
-                QString str = QString((const QChar *)m, len/2);
+                QString str;
+                if (len != -1)
+                    str = QString((const QChar *)m, len / 2);
                 if (QSysInfo::ByteOrder == QSysInfo::LittleEndian) {
                     for (int i = 0; i < str.length(); ++i)
                         str[i] = QChar((str.at(i).unicode() >> 8) +

tests/auto/linguist/lconvert/tst_lconvert.cpp
@@ -282,6 +282,7 @@ void tst_lconvert::roundtrips_data()
     QStringList tsPoTs; tsPoTs << "ts" << "po" << "ts";
     QStringList tsXlfTs; tsXlfTs << "ts" << "xlf" << "ts";
     QStringList tsQmTs; tsQmTs << "ts" << "qm" << "ts";
+    QStringList qmTsQm; qmTsQm << "qm" << "ts" << "qm";
 
     QList<QStringList> noArgs;
     QList<QStringList> filterPoArgs; filterPoArgs << QStringList() << (QStringList() << "-drop-tag" << "po:*");
@@ -318,6 +319,7 @@ void tst_lconvert::roundtrips_data()
 
     QTest::newRow("ts-po-ts (endless loop)") << "endless-po-loop.ts" << tsPoTs << noArgs;
     QTest::newRow("ts-qm-ts (whitespace)") << "whitespace.ts" << tsQmTs << noArgs;
+    QTest::newRow("qm-ts-qm (untranslated)") << "untranslated.qm" << qmTsQm << noArgs;
 }
 
 void tst_lconvert::roundtrips()