c8f59c7aecc1216c60699b6a1d69a336a9b7f3e4
Kai Köhne <kai.koehne@qt.io>
2021-10-20 16:04:07 +0200

    windeployqt: Drop mentioning of ICU from help output
    We don't by default build anymore with ICU since quite a while.
    
    Change-Id: I24efbb7d5355843e444c7d90e3ea67410f306c32
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/windeployqt/main.cpp
+++ src/windeployqt/main.cpp
@@ -309,8 +309,7 @@ static inline int parseArguments(const QStringList &arguments, QCommandLineParse
     parser->setSingleDashWordOptionMode(QCommandLineParser::ParseAsLongOptions);
     parser->setApplicationDescription(QStringLiteral("Qt Deploy Tool ") + QLatin1String(QT_VERSION_STR)
         + QLatin1String("\n\nThe simplest way to use windeployqt is to add the bin directory of your Qt\n"
-        "installation (e.g. <QT_DIR\\bin>) to the PATH variable and then run:\n  windeployqt <path-to-app-binary>\n"
-        "If ICU, etc. are not in the bin directory, they need to be in the PATH\nvariable. "
+        "installation (e.g. <QT_DIR\\bin>) to the PATH variable and then run:\n  windeployqt <path-to-app-binary>\n\n"
         "If your application uses Qt Quick, run:\n  windeployqt --qmldir <path-to-app-qml-files> <path-to-app-binary>"));
     const QCommandLineOption helpOption = parser->addHelpOption();
     parser->addVersionOption();
