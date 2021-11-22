915761952f8c40131c1966f6a204660c4acc2715
Joerg Bornemann <joerg.bornemann@qt.io>
2021-11-22 10:06:33 +0100

    macdeployqt: Don't write Paths/Imports to qt.conf
    This key is outdated.
    
    Change-Id: I106980cecd63b9929551cbc81cf39f91a813a8ef
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Craig Scott <craig.scott@qt.io>

--- src/macdeployqt/shared/shared.cpp
+++ src/macdeployqt/shared/shared.cpp
@@ -1204,7 +1204,6 @@ void createQtConf(const QString &appBundlePath)
     // Set Plugins and imports paths. These are relative to App.app/Contents.
     QByteArray contents = "[Paths]\n"
                           "Plugins = PlugIns\n"
-                          "Imports = Resources/qml\n"
                           "Qml2Imports = Resources/qml\n";
 
     QString filePath = appBundlePath + "/Contents/Resources/";
