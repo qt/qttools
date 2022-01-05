2a46b6d4eff743d11cba991a381736a9a159e786
Kai Köhne <kai.koehne@qt.io>
2022-01-05 12:01:13 +0100

    Bump copyright year to 2022
    Change-Id: I1b3f0d1b84f507e45728a00fa652d0db936cebca
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit bd8827cd95552372a5aca78a21210a46bddadc36)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/designer/src/designer/versiondialog.cpp
+++ src/designer/src/designer/versiondialog.cpp
@@ -160,7 +160,7 @@ VersionDialog::VersionDialog(QWidget *parent)
 
     lbl->setText(tr("%1"
                     "<br/>Copyright (C) %2 The Qt Company Ltd."
-                    ).arg(version, QStringLiteral("2021")));
+                    ).arg(version, QStringLiteral("2022")));
 
     lbl->setWordWrap(true);
     lbl->setOpenExternalLinks(true);
