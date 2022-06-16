45dc9fa0221a3aadc9925eead484f012a62a6a58
Albert Astals Cid <aacid@kde.org>
2022-06-16 09:52:54 +0200

    Ensure FileAttributeSetTable is filled ordered so we get the same qch file each time
    This is part of making all of Qt reproducible
    
    Change-Id: I1a4120cab6844887fb4b48edb238b8f1b55f0eb9
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/assistant/qhelpgenerator/helpgenerator.cpp
+++ src/assistant/qhelpgenerator/helpgenerator.cpp
@@ -411,7 +411,9 @@ bool HelpGeneratorPrivate::insertFiles(const QStringList &files, const QString &
     if (filterSetId < 0)
         return false;
     ++filterSetId;
-    for (int attId : qAsConst(filterAtts)) {
+    QList<int> attValues = filterAtts.values();
+    std::sort(attValues.begin(), attValues.end());
+    for (int attId : qAsConst(attValues)) {
         m_query->prepare(QLatin1String("INSERT INTO FileAttributeSetTable "
             "VALUES(?, ?)"));
         m_query->bindValue(0, filterSetId);
