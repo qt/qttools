fffdb06383ff9aa29bcd1988cb19ca3e927bd224
Venugopal Shivashankar <Venugopal.Shivashankar@qt.io>
2022-08-16 12:10:37 +0200

    doc: Handle macros while generating since info
    The generator treated macros as a function/method, resulting
    in a since entry that referred to the macro as a function.
    
    In addition, updated test data to ensure that the since entry
    for a \macro is tested.
    
    Change-Id: I648511705babad84163bc3e0ebd8233c4d46f4e2
    Pick-to: 6.4 6.3 6.2
    Fixes: QTBUG-105097
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/qdoc/generator.cpp
+++ src/qdoc/generator.cpp
@@ -2101,6 +2101,9 @@ QString Generator::typeString(const Node *node)
         case FunctionNode::JsMethod:
         case FunctionNode::QmlMethod:
             return "method";
+        case FunctionNode::MacroWithParams:
+        case FunctionNode::MacroWithoutParams:
+            return "macro";
         default:
             break;
         }
