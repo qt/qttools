1f859ca1ac6d337aedde2fe8eed74266416ae0d1
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-02-08 10:27:41 +0100

    Qt Designer: Remove worldtimeclock examples
    The functionality is sufficiently demonstrated by the existing custom
    widget plugin examples.
    
    Task-number: QTBUG-110447
    Pick-to: 6.5
    Change-Id: I09198bba46ebe8da03c9e21192588e6010bec541
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- examples/designer/customwidgetplugin/analogclock.h
+++ examples/designer/customwidgetplugin/analogclock.h
@@ -7,15 +7,19 @@
 #include <QWidget>
 #include <QtUiPlugin/QDesignerExportWidget>
 
+//! [0]
 class QDESIGNER_WIDGET_EXPORT AnalogClock : public QWidget
 {
     Q_OBJECT
+//! [0]
 
 public:
     explicit AnalogClock(QWidget *parent = nullptr);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
+//! [1]
 };
+//! [1]
 
 #endif
