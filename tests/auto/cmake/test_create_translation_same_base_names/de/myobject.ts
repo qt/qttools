c04dd17219a80edff46ea87fe2f4f9fe1d3496fe
Joerg Bornemann <joerg.bornemann@qt.io>
2023-04-25 10:14:54 +0200

    CMake: Fix qt5_create_translation for same-named .ts files
    Consider a project where .ts files are structured by directory instead
    of by file name:
    
    translations/chinese/myapp.ts
    translations/czech/myapp.ts
    translations/french/myapp.ts
    translations/german/myapp.ts
    translations/italian/myapp.ts
    
    In that case, qt5_create_translation failed at configure time with the
    following error:
    
      Attempt to add a custom rule to output
    
        /build/myapp/.lupdate/myapp.ts.stamp.rule
    
      which already has a custom rule.
    
    Fix that by constructing a .ts.stamp file path that takes the relative
    path from CMAKE_CURRENT_SOURCE_DIR to the .ts file into account.
    
    Additionally, pass the paths of the timestamp files to
    qt5_add_translation with the __QT_INTERNAL_TIMESTAMP_FILES multi-value
    argument to avoid having to recalculate the paths of the timestamp
    files.
    
    Fixes: QTBUG-113152
    Change-Id: Ifa08741cd621f2215eaf2f8fb848ecf6d5b9cf13
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit d2eaccfe7f613faa2083277d2d43c10c85053126)

---
+++ tests/auto/cmake/test_create_translation_same_base_names/de/myobject.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../myi18nobject.cpp" line="26"/>
+        <source>Hello, world!</source>
+        <translation>Hallo, Welt!</translation>
+    </message>
+</context>
+</TS>
