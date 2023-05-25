c3feee64550c42aacd4b39bc180c34b63f010fc2
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-05-25 13:47:12 +0200

    Qt Designer: Replace a QHash<QObject *,bool> by QSet<QObject *>
    Pick-to: 6.5
    Change-Id: I6f18597d7777a4cacac1e6cb5d2c2059a00e868f
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/designer/src/lib/extension/default_extensionfactory.cpp
+++ src/designer/src/lib/extension/default_extensionfactory.cpp
@@ -89,7 +89,7 @@ QObject *QExtensionFactory::extension(QObject *object, const QString &iid) const
 
     if (!m_extended.contains(object)) {
         connect(object, &QObject::destroyed, this, &QExtensionFactory::objectDestroyed);
-        m_extended.insert(object, true);
+        m_extended.insert(object);
     }
 
     if (it == m_extensions.end())
