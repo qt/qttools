0b97d519205b73553708e7b5f99b5ac25dea3e25
Tarja Sundqvist <tarja.sundqvist@qt.io>
2023-10-24 12:24:33 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the files in tqtc-qttools. Examples, tests, or
    documentation files are not updated.
    
    Task-number: QTQAINFRA-5900
    Change-Id: I15912db714269e57ad620f2277d08ba9f8b3bdcd
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/linguist/Qt6LinguistToolsMacros.cmake
+++ src/linguist/Qt6LinguistToolsMacros.cmake
@@ -1,6 +1,6 @@
 # Copyright (C) 2020 The Qt Company Ltd.
 # Copyright 2005-2011 Kitware, Inc.
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 include(CMakeParseArguments)
 
--- src/linguist/lupdate/main.cpp
+++ src/linguist/lupdate/main.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Marc Mutz <marc.mutz@kdab.com>
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #include "lupdate.h"
 #if QT_CONFIG(clangcpp)
