951a9dab91e8982fd8e6b01bce9ff58e2028aa1b
Paul Wicking <paul.wicking@qt.io>
2023-10-26 08:36:14 +0200

    QDoc: Respect line continuation characters
    Since a refactoring (d7eeb6b0f78781cac8b87f4f18894aca6a467ae6), QDoc no
    longer parses trailing backslashes immediately preceding a newline
    character as line continuation character correctly, in that content in
    the continued line is duplicated. This is due to the refactoring not
    properly updating the position.
    
    The refactoring caused another side effect, addressed in
    d988115e061622e2c7728e83295727be9303a1c3. That change caused a new side
    effect that may cause leading and trailing whitespace characters in a
    continued line to end up verbatim in the continued line.
    
    This change seeks to address these two problems in the following way:
    - Ensure the main loop of the method updates the starting position
      for the line of text at the end of each iteration.
    - If a trailing backslash has been encountered, return a simplified
      string. This ensures that sequences of internal whitespace is
      replaced by a single space character.
    - Change a local variable name to better reflect what it represents.
    - Update the documentation for the method to account for the behavior
      change.
    
    The reintroduction of `simplified()` return values may possibly have
    unintentional consequences. For example, it may impact parsing of
    regular expressions, as seen as side-effect of the original
    refactoring.
    
    Fixes: QTBUG-118558
    Pick-to: 6.6
    Change-Id: I5bbf51b1923a5e04f324c3f92aeb1cceff8aab7c
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

---
+++ src/qdoc/qdoc/tests/generatedoutput/testdata/trailing_backslashes/trailing_backslashes.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GFDL-1.3-no-invariants-only
+
+/*!
+    \struct Struct
+*/
+
+/*!
+    \fn void Struct::oneTrailingSlash()
+    \keyword one \
+    slash
+
+    \sa {two slashes again}
+*/
+
+/*!
+    \fn void Struct::MultipleTrailingSlashes()
+    \keyword two \
+             slashes \
+             here
+
+    \sa {one slash}
+*/
+
+/*!
+    \fn void Struct::multipleTrailingSlashesAndRandomWhitespace()
+    \keyword   two\
+    slashes \
+    again
+
+    \sa {two slashes here}
+*/
