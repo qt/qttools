6fbd6e1f776b1a23b392cc639e01ac26898444f6
Joerg Bornemann <joerg.bornemann@qt.io>
2023-11-08 15:56:10 +0100

    CMake: Fix the XXX_lupdate targets for Visual Studio generators
    When using the VS generator, the foo_lupdate targets were run on every
    build, which is neither expected nor wanted. The reason for this is a
    pecularity in MSBuild and as a reaction to this, special behavior of
    CMake: add_dependencies(A B) will enable B in the default build of the
    solution - even if B is not triggered by the ALL target. See upstream
    CMake issue #16668 for details.
    
    We now work around this problem by building foo_lupdate externally as
    PRE_BUILD step of update_translations.
    
    Pick-to: 6.5
    Fixes: QTBUG-115166
    Change-Id: I33110450de6289f99954890ff8785ecbe67e5a29
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 0338a2f7523e1fe21be325e76f6aba6b0c9e4467)
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

--- src/linguist/Qt6LinguistToolsMacros.cmake
+++ src/linguist/Qt6LinguistToolsMacros.cmake
@@ -198,7 +198,23 @@ set(lupdate_translations \"${ts_files}\")
         if(NOT TARGET ${QT_GLOBAL_LUPDATE_TARGET})
             add_custom_target(${QT_GLOBAL_LUPDATE_TARGET})
         endif()
-        add_dependencies(${QT_GLOBAL_LUPDATE_TARGET} ${target}_lupdate)
+        if(CMAKE_GENERATOR MATCHES "^Visual Studio ")
+            # For the Visual Studio generators we cannot use add_dependencies, because this would
+            # enable ${target}_lupdate in the default build of the solution. See QTBUG-115166 and
+            # upstream CMake issue #16668 for details. As a work-around, we run the
+            # ${target}_lupdate through 'cmake --build' as PRE_BUILD step of the global lupdate
+            # target.
+            add_custom_command(
+                TARGET ${QT_GLOBAL_LUPDATE_TARGET} PRE_BUILD
+                COMMAND "${CMAKE_COMMAND}" --build "${CMAKE_BINARY_DIR}" -t ${target}_lupdate
+            )
+
+            # Exclude ${target}_lupdate from the solution's default build to avoid it being enabled
+            # should the user add a dependency to it.
+            set_property(TARGET ${target}_lupdate PROPERTY EXCLUDE_FROM_DEFAULT_BUILD ON)
+        else()
+            add_dependencies(${QT_GLOBAL_LUPDATE_TARGET} ${target}_lupdate)
+        endif()
     endif()
 endfunction()
 
