436affa3ce95df8c5c042de91c4ddf522e0cae20
Luca Di Sera <luca.disera@qt.io>
2023-11-27 12:27:50 +0100

    QDoc: Allow RelaxedTemplateDeclaration to be rebuilt as string
    When QDoc parses a project, it parses the source code to extract the
    user-provided documentation and perform sanity checkings based on the
    code itself on it.
    
    When QDoc parses an "\fn" command as part of this process, it tries to
    understand, based on its intermediate representation built on the
    information extracted from the code-base, which "documentable element"
    the "\fn" refers to.
    
    When QDoc performs this "matching" process, it takes into consideration
    only a certain amount of information.
    For example, no checking is performed over the template declaration of a
    callable.
    
    Due to QDoc ignoring template declaration when "matching", certain
    elements are not documentable, as they are indistinguishable.
    
    For example, two callables that are overloaded on their specialization
    will be treated as a single documentable element by QDoc, so that they
    cannot both be referred to when documenting the code-base.
    
    Currently, QDoc extracts template declarations from templated elements
    directly from Clang, and stores them whole as a stringified
    representation.
    
    This representation allows QDoc to only take into consideration a whole
    template declaration, as the representation lacks granularity.
    
    Due to this representation, QDoc does not have, for example, easy access
    to the information about single template parameters in a declaration.
    
    This lack of granularity, in turn, prevents QDoc from performing more
    complex operations on a template declaration.
    For example, the representation would not easily allow QDoc to
    understand if a documentation block mentions or not the name of a
    template parameter, similar to what is done for callables parameters by
    the "\a" command.
    
    Similarly, it would only allow QDoc to perform "matching" on the whole
    declaration, which might be different under the current retrieval
    process when extracted from the code (where it is extracted from a
    declaration) or from an "\fn" command (where it is extracted from a mock
    out-of-line definition).
    
    To simplify the implementation of features related to template
    declaration, such as taking them into consideration when matching or
    allowing the user to document template parameters in a sanity-checked
    way, a more granular, albeit very simplified, representation for
    template declarations was recently added to QDoc in
    "template_declaration.h".
    
    The representation is intended to soon replace our current whole-string
    representation for template declarations.
    
    As template declarations are shown to the user in the final output
    documentation, our internal representations require us to be able to
    rebuild a relatively correct human-presentable version that can be used
    when the output documentation is generated.
    
    Hence, a way to rebuild a stringified human-presentable version of the
    new representation for template declarations in "template_declaration.h"
    is provided.
    
    Each relevant type in "template_declaration.h" was enhanced with a
    "to_std_string" method.
    
    The method takes care of rebuilding a human-presentable version of the
    code that is modeled by the representation that can be shown to a user,
    so that the new representation can be plugged in place of the current
    one.
    
    Task-number: QTBUG-118080
    Task-number: QTBUG-117881
    Change-Id: Ief791a7387d116ff05276ced6b0cbdcf71c1336d
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

--- src/qdoc/qdoc/src/qdoc/template_declaration.h
+++ src/qdoc/qdoc/src/qdoc/template_declaration.h
@@ -3,10 +3,13 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cstdint>
 #include <optional>
 #include <string>
 #include <vector>
+
+#include <QString>
 
 /*
  * Represents a general declaration that has a form that can be
@@ -51,9 +54,94 @@
  */
 struct ValuedDeclaration
 {
+    struct PrintingPolicy
+    {
+        bool include_type = true;
+        bool include_name = true;
+        bool include_initializer = true;
+    };
+
     std::string type;
     std::string name;
     std::string initializer;
+
+    // KLUDGE: Workaround for
+    // https://stackoverflow.com/questions/53408962/try-to-understand-compiler-error-message-default-member-initializer-required-be
+    static PrintingPolicy default_printing_policy() { return PrintingPolicy{}; }
+
+    /*
+     * Constructs and returns a human-readable representation of this
+     * declaration.
+     *
+     * The constructed string is formatted so that as to rebuild a
+     * possible version of the C++ code that is modeled by an instance
+     * of this type.
+     *
+     * Each component participates in the human-presentable version if
+     * it they are not the empty string.
+     *
+     * The "type" and "name" component participate with their literal
+     * representation.
+     *
+     * The "iniitlalizer" components contributes an equal symbol,
+     * followed by a space followed by the literal representation of
+     * the component.
+     *
+     * The component contributes in an ordered way, with "type"
+     * contributing first, "name" contributing second and
+     * "initializer" contributing last.
+     *
+     * Each contribution is separated by a space if the component that
+     * comes before it, if any, has contributed to the human-readable
+     * representation.
+     *
+     * For example, an instance of this type that has "type" component
+     * "int", "name" component "foo" and "iniitializer" component
+     * "100", would be represented as:
+     *
+     *  int foo = 100
+     *
+     *  Where "int" is the "type" component contribution, "foo" is the
+     *  "name" component contribution and "= 100" is the "initializer"
+     *  component contribution.
+     *  Each of those contribution is separated by a space, as each
+     *  "preceding" component has contributed to the representation.
+     *
+     *  If we provide a similar instance with, for example, the "type"
+     *  and "name" components as the empty string, then the
+     *  representation would be "= 100", which is the "initializer"
+     *  component contribution, the only component that is not the
+     *  empty string.
+     *
+     *  The policy argument allows to treat certain components as if
+     *  they were the empty string.
+     *
+     *  For example, given an instance of this type that has "type"
+     *  component "double", "name" component "bar" and "iniitializer"
+     *  component "10.2", its human-readable representation would be
+     *  "double bar = 10.2".
+     *
+     *  If the representation of that same instance was obtained by
+     *  using a policy that excludes the "name" component, then that
+     *  representation would be "double = 10.2", which is equivalent
+     *  to the representation of an instance that is the same as the
+     *  orginal one with the "name" component as the empty string.
+     */
+    inline std::string to_std_string(PrintingPolicy policy = default_printing_policy()) const
+    {
+        std::string s{};
+
+        if (!type.empty() && policy.include_type)
+            s += (s.empty() ? "" : " ") + type;
+
+        if (!name.empty() && policy.include_name)
+            s += (s.empty() ? "" : " ") + name;
+
+        if (!initializer.empty() && policy.include_initializer)
+            s += (s.empty() ? "= " : " = ") + initializer;
+
+        return s;
+    }
 };
 
 struct RelaxedTemplateParameter;
@@ -61,6 +149,8 @@ struct RelaxedTemplateParameter;
 struct TemplateDeclarationStorage
 {
     std::vector<RelaxedTemplateParameter> parameters;
+
+    inline std::string to_std_string() const;
 };
 
 /*
@@ -143,6 +233,162 @@ struct RelaxedTemplateParameter
     bool is_parameter_pack;
     ValuedDeclaration valued_declaration;
     std::optional<TemplateDeclarationStorage> template_declaration;
+
+    /*
+     * Constructs and returns a human-readable representation of this
+     * parameter.
+     *
+     * The constructed string is formatted so that as to rebuild a
+     * possible version of the C++ code that is modeled by an instance
+     * of this type.
+     *
+     * The format of the representation varies based on the "kind" of
+     * the parameter.
+     *
+     *   - A "TypeTemplateParameter", is constructed as the
+     *     concatenation of the literal "typename", followed by the
+     *     literal "..." if the parameter is a pack, followed by the
+     *      human-readable representaion of "valued_declaration".
+     *
+     *      If the human-readable representation of
+     *      "valued_declaration" is not the empty string, it is
+     *      preceded by a space when it contributes to the
+     *      representation.
+     *
+     *      For example, the C++ type template parameter "typename Foo
+     *      = int", would be represented by the instance:
+     *
+     *        RelaxedTemplateParameter{
+     *            RelaxedTemplateParameter::Kind::TypeTemplateParameter,
+     *            false,
+     *            {
+     *                "",
+     *                "Foo",
+     *                "int"
+     *            },
+     *            {}
+     *        };
+     *
+     *      And its representation would be:
+     *
+     *        typename Foo = int
+     *
+     *      Where "typename" is the added literal and "Foo = int" is
+     *      the representation for "valued_declaration", with a space
+     *      in-between the two contributions.
+     *
+     *    - A "NonTypeTemplateParameter", is constructed by the
+     *      contribution of the "type" compoment of "valued_declaration",
+     *      followed by the literal "..." if the parameter is a pack,
+     *      followed by the human-presentable version of
+     *      "valued_declaration" without its "type" component
+     *      contribution.
+     *
+     *      If the contribution of the "type" component of
+     *      "valued_declaration" is not empty, the next contribution is
+     *      preceded by a space.
+     *
+     *      For example, the C++ non-type template parameter "int...
+     *      SIZE", would be represented by the instance:
+     *
+     *
+     *        RelaxedTemplateParameter{
+     *            RelaxedTemplateParameter::Kind::NonTypeTemplateParameter,
+     *            true,
+     *            {
+     *                "int",
+     *                "SIZE",
+     *                ""
+     *            },
+     *            {}
+     *        };
+     *
+     *      And its representation would be:
+     *
+     *        int... SIZE
+     *
+     *      Where "int" is the "type" component contribution of
+     *      "valued_declaration", "..." is the added literal due to
+     *      the parameter being a pack and " SIZE" being the
+     *      human-readable representation of "valued_declaration"
+     *      without its "type" component contribution, preceded by a
+     *      space.
+     *
+     *    - A "TemplateTemplateParameter", is constructed by the
+     *      contribution of the human-presentable representation of
+     *      "template_declaration", followed by the representation of
+     *      this parameter if it was a "TypeTemplateParameter", with a
+     *      space between the two contributions if the
+     *      human-presentable representation of "template_declaration"
+     *      is not empty.
+     *
+     *      For example, the C++ template template template parameter
+     *      "template<typename> T", would be represented by the
+     *      instance:
+     *
+     *
+     *        RelaxedTemplateParameter{
+     *            RelaxedTemplateParameter::Kind::TemplateTemplateParameter,
+     *            false,
+     *            {
+     *                "",
+     *                "T",
+     *                ""
+     *            },
+     *            {
+     *              RelaxedTemplateParameter{
+     *                RelaxedTemplateParameter::Kind::TypeTemplateParameter,
+     *                false,
+     *                {
+     *                    "",
+     *                    "",
+     *                    ""
+     *                },
+     *                {}
+     *              }
+     *            }
+     *        };
+     *
+     *      And its representation would be:
+     *
+     *        template <typename> typename T
+     *
+     *      Where "template <typename>" human-presentable version of
+     *      "template_declaration" and "typename T" is the
+     *      human-presentable version of this parameter if it was a
+     *      type template parameter.
+     *
+     *      With a space between the two contributions.
+     */
+    inline std::string to_std_string() const
+    {
+        switch (kind) {
+        // TODO: This can probably be moved under the template
+        // template parameter case and reused through a fallback.
+        case Kind::TypeTemplateParameter: {
+            std::string valued_declaration_string = valued_declaration.to_std_string();
+
+            return std::string("typename") + (is_parameter_pack ? "..." : "")
+                    + (valued_declaration_string.empty() ? "" : " ") + valued_declaration_string;
+        }
+        case Kind::NonTypeTemplateParameter: {
+            std::string type_string = valued_declaration.type + (is_parameter_pack ? "..." : "");
+
+            return type_string + (type_string.empty() ? "" : " ")
+                    + valued_declaration.to_std_string(
+                            ValuedDeclaration::PrintingPolicy{ false, true, true });
+        }
+        case Kind::TemplateTemplateParameter: {
+            std::string valued_declaration_string = valued_declaration.to_std_string();
+
+            return (template_declaration ? (*template_declaration).to_std_string() + " " : "")
+                    + "typename" + (is_parameter_pack ? "..." : "")
+                    + (valued_declaration_string.empty() ? "" : " ") + valued_declaration_string;
+        }
+        default:
+            return "";
+        }
+    }
 };
 
 /*
@@ -158,4 +404,42 @@ struct RelaxedTemplateParameter
  * already validated and known to be correct, such as data that is
  * extracted from Clang.
  */
-struct RelaxedTemplateDeclaration : TemplateDeclarationStorage {};
+struct RelaxedTemplateDeclaration : TemplateDeclarationStorage
+{
+    inline QString to_qstring() const { return QString::fromStdString(to_std_string()); }
+};
+
+/*
+ * Constructs and returns a human-readable representation of this
+ * declaration.
+ *
+ * The constructed string is formatted so as to rebuild a
+ * possible version of the C++ code that is modeled by an instance
+ * of this type.
+ *
+ * The representation of a declaration is constructed by the literal
+ * "template <", followed by the human-presentable version of each
+ * parameter in "parameters", with a comma and a space between each
+ * parameter, followed by a closing literal ">".
+ *
+ * For example, the empty declaration is represented as "template <>".
+ *
+ * While a template declaration that has a type template parameter
+ * "Foo" with initializer "int" and a non-type template parameter pack
+ * with type "int" and name "S" would be represented as:
+ *
+ *   template <typename Foo = int, int... S>
+ */
+inline std::string TemplateDeclarationStorage::to_std_string() const
+{
+    if (parameters.empty())
+        return "template <>";
+
+    return "template <"
+            + std::accumulate(std::next(parameters.cbegin()), parameters.cend(),
+                              parameters.front().to_std_string(),
+                              [](auto &&acc, const RelaxedTemplateParameter &parameter) {
+                                  return acc + ", " + parameter.to_std_string();
+                              })
+            + ">";
+}
