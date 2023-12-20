67b706ee219d7ae59f5b1116ef12efd8db7e953f
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-12-20 09:43:24 +0100

    Qt Designer: Change the settings keys to be latin1 string literals
    Task-number: QTBUG-120236
    Change-Id: I9ec291672afc31900c74833367a27ff460dba848
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>
    (cherry picked from commit 27dc5ba5a9c96b6e1c3ffdf538fcc815f2e0fd65)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 29a4460ba942f18609bb6464c1b1470942f1c60b)

--- src/designer/src/designer/qdesigner_settings.cpp
+++ src/designer/src/designer/qdesigner_settings.cpp
@@ -26,15 +26,15 @@ QT_BEGIN_NAMESPACE
 
 using namespace Qt::StringLiterals;
 
-static const char newFormShowKey[] = "newFormDialog/ShowOnStartup";
+static constexpr auto newFormShowKey = "newFormDialog/ShowOnStartup"_L1;
 
 // Change the version whenever the arrangement changes significantly.
-static const char mainWindowStateKey[] = "MainWindowState45";
-static const char toolBarsStateKey[] = "ToolBarsState45";
+static constexpr auto mainWindowStateKey = "MainWindowState45"_L1;
+static constexpr auto toolBarsStateKey = "ToolBarsState45"_L1;
 
-static const char backupOrgListKey[] = "backup/fileListOrg";
-static const char backupBakListKey[] = "backup/fileListBak";
-static const char recentFilesListKey[] = "recentFilesList";
+static constexpr auto backupOrgListKey = "backup/fileListOrg"_L1;
+static constexpr auto backupBakListKey = "backup/fileListBak"_L1;
+static constexpr auto recentFilesListKey = "recentFilesList"_L1;
 
 QDesignerSettings::QDesignerSettings(QDesignerFormEditorInterface *core) :
     qdesigner_internal::QDesignerSharedSettings(core)
@@ -98,44 +98,44 @@ void QDesignerSettings::restoreGeometry(QWidget *w, QRect fallBack) const
 
 QStringList QDesignerSettings::recentFilesList() const
 {
-    return settings()->value(QLatin1StringView(recentFilesListKey)).toStringList();
+    return settings()->value(recentFilesListKey).toStringList();
 }
 
 void QDesignerSettings::setRecentFilesList(const QStringList &sl)
 {
-    settings()->setValue(QLatin1StringView(recentFilesListKey), sl);
+    settings()->setValue(recentFilesListKey, sl);
 }
 
 void QDesignerSettings::setShowNewFormOnStartup(bool showIt)
 {
-    settings()->setValue(QLatin1StringView(newFormShowKey), showIt);
+    settings()->setValue(newFormShowKey, showIt);
 }
 
 bool QDesignerSettings::showNewFormOnStartup() const
 {
-    return settings()->value(QLatin1StringView(newFormShowKey), true).toBool();
+    return settings()->value(newFormShowKey, true).toBool();
 }
 
 QByteArray QDesignerSettings::mainWindowState(UIMode mode) const
 {
-    return settings()->value(QLatin1StringView(mainWindowStateKey) + modeChar(mode)).toByteArray();
+    return settings()->value(mainWindowStateKey + modeChar(mode)).toByteArray();
 }
 
 void QDesignerSettings::setMainWindowState(UIMode mode, const QByteArray &mainWindowState)
 {
-    settings()->setValue(QLatin1StringView(mainWindowStateKey) + modeChar(mode), mainWindowState);
+    settings()->setValue(mainWindowStateKey + modeChar(mode), mainWindowState);
 }
 
 QByteArray QDesignerSettings::toolBarsState(UIMode mode) const
 {
-    QString key = QLatin1StringView(toolBarsStateKey);
+    QString key = toolBarsStateKey;
     key += modeChar(mode);
     return settings()->value(key).toByteArray();
 }
 
 void QDesignerSettings::setToolBarsState(UIMode mode, const QByteArray &toolBarsState)
 {
-    QString key = QLatin1StringView(toolBarsStateKey);
+    QString key = toolBarsStateKey;
     key += modeChar(mode);
     settings()->setValue(key, toolBarsState);
 }
@@ -143,8 +143,8 @@ void QDesignerSettings::setToolBarsState(UIMode mode, const QByteArray &toolBars
 void QDesignerSettings::clearBackup()
 {
     QDesignerSettingsInterface *s = settings();
-    s->remove(QLatin1StringView(backupOrgListKey));
-    s->remove(QLatin1StringView(backupBakListKey));
+    s->remove(backupOrgListKey);
+    s->remove(backupBakListKey);
 }
 
 void QDesignerSettings::setBackup(const QMap<QString, QString> &map)
@@ -153,14 +153,14 @@ void QDesignerSettings::setBackup(const QMap<QString, QString> &map)
     const QStringList bak = map.values();
 
     QDesignerSettingsInterface *s = settings();
-    s->setValue(QLatin1StringView(backupOrgListKey), org);
-    s->setValue(QLatin1StringView(backupBakListKey), bak);
+    s->setValue(backupOrgListKey, org);
+    s->setValue(backupBakListKey, bak);
 }
 
 QMap<QString, QString> QDesignerSettings::backup() const
 {
-    const QStringList org = settings()->value(QLatin1StringView(backupOrgListKey), QStringList()).toStringList();
-    const QStringList bak = settings()->value(QLatin1StringView(backupBakListKey), QStringList()).toStringList();
+    const QStringList org = settings()->value(backupOrgListKey, QStringList()).toStringList();
+    const QStringList bak = settings()->value(backupBakListKey, QStringList()).toStringList();
 
     QMap<QString, QString> map;
     const qsizetype orgCount = org.size();
