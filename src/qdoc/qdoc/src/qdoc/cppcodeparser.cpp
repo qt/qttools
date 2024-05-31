55a3d80f089fb63a8f89def95b84282691e5a479
Moss Heim <moss.heim@qt.io>
2024-05-31 11:25:10 +0200

    QDoc: fix typo in warning
    Change-Id: I08ae3800f4657ab8e1f5a3b899b7185a3be7b607
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    (cherry picked from commit 981cb993419aa6728eca7dbe99ae9ac597e3caac)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qdoc/qdoc/src/qdoc/cppcodeparser.cpp
+++ src/qdoc/qdoc/src/qdoc/cppcodeparser.cpp
@@ -437,7 +437,8 @@ void CppCodeParser::processMetaCommand(const Doc &doc, const QString &command,
         }
     } else if (command == COMMAND_QMLNATIVETYPE || command == COMMAND_QMLINSTANTIATES) {
         if (command == COMMAND_QMLINSTANTIATES)
-            doc.location().report(u"\\instantiates is deprected and will be removed in a future version. Use \\nativetype instead."_s);
+            doc.location().report(
+                    u"\\instantiates is deprecated and will be removed in a future version. Use \\nativetype instead."_s);
         // TODO: COMMAND_QMLINSTANTIATES is deprecated since 6.8. Its remains should be removed no later than Qt 7.0.0.
         processQmlNativeTypeCommand(node, command, arg, doc.location());
     } else if (command == COMMAND_DEFAULT) {
