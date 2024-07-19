528a2d80ca4f502f26066dfe1b7d773bb308591a
Kai Köhne <kai.koehne@qt.io>
2024-07-19 10:25:51 +0200

    CMake: Add comment how to populate dummy .ts files
    Pick-to: 6.8
    Change-Id: Id1732bb66afa636cdfdd25c331b073c7f729ff79
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/linguist/Qt6LinguistToolsMacros.cmake
+++ src/linguist/Qt6LinguistToolsMacros.cmake
@@ -194,6 +194,9 @@ function(_qt_internal_ensure_ts_file)
 
     file(WRITE "${arg_TS_FILE}"
         [[<?xml version="1.0" encoding="utf-8"?>
+<!--
+Run the update_translations CMake target to populate the source strings in this file.
+-->
 <!DOCTYPE TS>
 <TS/>
 ]])
