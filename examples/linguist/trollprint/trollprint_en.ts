faa1c1e280a1721fdb5a46c1a2ecd27efc8ed88f
Joerg Bornemann <joerg.bornemann@qt.io>
2024-09-06 12:24:54 +0200

    Add initial plurals .ts files for i18n examples
    Since we automatically create a plurals .ts file with
    qt_add_translations, the i18n examples pollute the source tree of Qt
    with generated .ts files.
    
    Fix this by adding initial .ts files to the repository.
    
    Fixes: QTBUG-124709
    Change-Id: I875c8395a975f7182d0a10781d96a9a78b2c1e4a
    Reviewed-by:  Alexey Edelev <alexey.edelev@qt.io>
    (cherry picked from commit 49ee2de84d0b22613dbae5e748218c621638854e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 83bed60b6ade54643540c0dda40dc05cd8925b96)

---
+++ examples/linguist/trollprint/trollprint_en.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+When using CMake, run the update_translations CMake target
+to populate the source strings in this file.
+
+When using QMake, run the following command:
+    lupdate trollprint.pro
+-->
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US"/>
