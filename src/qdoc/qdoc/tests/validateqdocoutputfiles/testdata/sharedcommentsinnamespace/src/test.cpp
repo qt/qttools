eb237a034e1d0f0d8716e704d15873c76b0ac02f
Topi Reinio <topi.reinio@qt.io>
2024-09-09 12:14:52 +0000

    qdoc: Fix double entries for shared comment node in detailed description
    If multiple API functions are documented directly under a namespace and
    the functions share a QDoc comment block, duplicate entries for them
    were generated in the detailed description - once with the correct
    documentation, and a second time with empty docs.
    
    This happened because Sections::stdRefPageSwitch() was indiscriminantly
    called for both `Summary` and `Details` sections, and that function did
    not check whether the node being distributed is sharing a comment (for
    `Details`).
    
    Extract the recurring piece of code that tests for shared comment nodes
    as a static helper method, and use it in all call sites doing node
    distribution to various sections. This makes the output for namespace
    reference pages more uniform with ones for classes and QML types.
    
    Add a validatedqdocoutputfiles test case with expected output.
    
    Fixes: QTBUG-120186
    Change-Id: If9f2d4c006c19958a4948cd3dfd395d215b6c97f
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    (cherry picked from commit 1083d1d13d45942eb4eb2acf315fed3890c824fa)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ src/qdoc/qdoc/tests/validateqdocoutputfiles/testdata/sharedcommentsinnamespace/src/test.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include "test.h"
+
+/*! \module Mod */
+/*! \namespace Test
+    \inmodule Mod
+*/
+namespace Test {
+/*!
+    \fn void foo()
+    \fn void bar()
+
+    Two functions sharing a doc comment.
+*/
+}
