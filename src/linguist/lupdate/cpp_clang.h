5c95d69da3f1031d89e975fd8c03e3ece3c03888
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-23 07:21:39 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json and REUSE.toml accordingly.
    Removed unsued license text files under LICENSES.
    
    Task-number: QTBUG-136484
    Change-Id: I8d2db0369a3c5747ed818bc14165d3fefbb09452
    Reviewed-by: Kai Köhne <kai.koehne@qt.io>

--- src/linguist/lupdate/cpp_clang.h
+++ src/linguist/lupdate/cpp_clang.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2019 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef CLANG_CPP_H
 #define CLANG_CPP_H
--- src/qdoc/catch_generators/src/catch_generators/generators/path_generator.h
+++ src/qdoc/catch_generators/src/catch_generators/generators/path_generator.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2022 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 // TODO: Change the include paths to implicitly consider
 // `catch_generators` a root directory and change the CMakeLists.txt
