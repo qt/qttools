qt/qttools

commit 3a9bc99b1087c224ff25b105f498bec1de209af5

lupdate: add support for template specializations
lupdate fails to identify the context in a method with
template specialization. This patch adds some lines to
identify a method with template specialization, and
correctly parse its context.

Fixes: QTBUG-132832
Change-Id: I6829f898d174d8507587cbe2c9818218962c0b53
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/linguist/lupdate/cpp.cpp
@@ -1787,6 +1787,11 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
     bool yyTokColonSeen = false; // Start of c'tor's initializer list
     bool yyTokIdentSeen = false; // Start of initializer (member or base class)
     bool maybeInTrailingReturnType = false;
+
+    const QList<TokenType> allowedTokensInFnTemplate{
+        Tok_Ident,   Tok_decltype,         Tok_ColonColon,       Tok_Comma,
+        Tok_Integer, Tok_LeftAngleBracket, Tok_RightAngleBracket
+    };
     metaExpected = true;
 
     prospectiveContext.clear();
@@ -2040,13 +2045,29 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
             modifyNamespace(&namespaces)->hasTrFunctions = true;
             yyTok = getToken();
             break;
-        case Tok_Ident:
+        case Tok_Ident: {
             if (yyTokColonSeen &&
                 yyBraceDepth == namespaceDepths.size() && yyParenDepth == 0) {
                 // member or base class identifier
                 yyTokIdentSeen = true;
             }
             yyTok = getToken();
+            QString className;
+            bool methodSpecialization = false;
+            if (yyTok == Tok_LeftAngleBracket) { // maybe a method specialization
+                int count = 1;
+                className = yyWord;
+                className.detach();
+                yyTok = getToken();
+                while (count && allowedTokensInFnTemplate.contains(yyTok)) {
+                    if (yyTok == Tok_LeftAngleBracket)
+                        count++;
+                    else if (yyTok == Tok_RightAngleBracket)
+                        count--;
+                    yyTok = getToken();
+                }
+                methodSpecialization = count == 0;
+            }
             if (yyTok == Tok_LeftParen) {
                 if (parseTranslate(prefix)) {
                     yyTok = getToken();
@@ -2056,13 +2077,14 @@ void CppParser::parseInternal(ConversionData &cd, const QStringList &includeStac
                 }
             }
             if (yyTok == Tok_ColonColon && !maybeInTrailingReturnType && !yyTrailingSpace) {
-                prefix += yyWord;
+                prefix += methodSpecialization ? className : yyWord;
                 prefix.detach();
             } else {
                 prefix.clear();
             }
             metaExpected = false;
             break;
+        }
         case Tok_Arrow:
             if (yyParenDepth == 0 && yyBraceDepth == namespaceDepths.size())
                 maybeInTrailingReturnType = true;

tests/auto/linguist/lupdate/testdata/good/parsecpp_template/project.ts.result
@@ -2,6 +2,30 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>ClassWithTemplateFn</name>
+    <message>
+        <location filename="template_classes.cpp" line="444"/>
+        <source>ClassWithTemplateFn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TemplateClassWithFn</name>
+    <message>
+        <location filename="template_classes.cpp" line="472"/>
+        <source>TemplateClassWithFn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TemplateClassWithTemplateFn</name>
+    <message>
+        <location filename="template_classes.cpp" line="459"/>
+        <source>TemplateClassWithTemplateFn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>cl1</name>
     <message>
         <location filename="template_classes.cpp" line="12"/>
@@ -397,6 +421,22 @@
     <message>
         <location filename="template_classes.cpp" line="416"/>
         <source>func4</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ns::NsTemplateClassWithFn</name>
+    <message>
+        <location filename="template_classes.cpp" line="497"/>
+        <source>NsTemplateClassWithFn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ns::NsTemplateClassWithTemplateFn</name>
+    <message>
+        <location filename="template_classes.cpp" line="511"/>
+        <source>NsTemplateClassWithTemplateFn</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

tests/auto/linguist/lupdate/testdata/good/parsecpp_template/template_classes.cpp
@@ -430,3 +430,83 @@ struct A
 struct B : A<QT_TRANSLATE_NOOP("context", "text")>
 {
 };
+
+struct ClassWithTemplateFn
+{
+    Q_OBJECT
+    template <typename T>
+    void fn();
+};
+
+template <>
+void ClassWithTemplateFn::fn<bool>()
+{
+    tr("ClassWithTemplateFn");
+}
+
+template <typename ClassType>
+struct TemplateClassWithTemplateFn
+{
+    Q_OBJECT
+    template <typename T>
+    void fn();
+};
+
+template <>
+template <>
+void TemplateClassWithTemplateFn<int>::fn<bool>()
+{
+    tr("TemplateClassWithTemplateFn");
+}
+
+template <typename ClassType>
+struct TemplateClassWithFn
+{
+    Q_OBJECT
+    void fn();
+};
+
+template <>
+void TemplateClassWithFn<int>::fn()
+{
+    tr("TemplateClassWithFn");
+}
+
+namespace ns {
+
+template <typename ClassType>
+struct NsTemplateClassWithFn
+{
+    Q_OBJECT
+    void fn();
+};
+
+template <typename ClassType>
+struct NsTemplateClassWithTemplateFn
+{
+    Q_OBJECT
+    template <typename T>
+    void fn();
+};
+
+} // namespace ns
+
+template <>
+void ns::NsTemplateClassWithFn<int>::fn()
+{
+    tr("NsTemplateClassWithFn");
+}
+
+#include <QDebug>
+struct Streamer
+{
+    QDebug get();
+};
+
+template <>
+template <>
+void ns::NsTemplateClassWithTemplateFn<int>::fn<bool>()
+{
+    Streamer str;
+    str.get() << tr("NsTemplateClassWithTemplateFn");
+}

commit 49a442e6a73608b80d96bccab6753db805600b0f

lupdate: detect left shift tokens
This patch amends to 3a9bc99b1087c224ff25b105f498bec1de209af5
which introduced a bug where in the case of ostream operator
after an identifier and before a namespaced tr command, the
namespace goes missing. This patch brings the detection of
ostream (left shift) token to lupdate to avoid such mistakes.

Fixes: QTBUG-132832
Change-Id: I7ded96f40091e83a1912302bf40595a3bd9308c9
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/linguist/lupdate/cpp.cpp
@@ -122,6 +122,7 @@ private:
         Tok_RightBracket,
         Tok_LeftAngleBracket,
         Tok_RightAngleBracket,
+        Tok_LeftShift,
         Tok_QuestionMark,
         Tok_LeftBrace,
         Tok_RightBrace,
@@ -831,6 +832,10 @@ CppParser::TokenType CppParser::getToken()
                 return Tok_RightAngleBracket;
             case '<':
                 yyCh = getChar();
+                if (yyCh == '<') {
+                    yyCh = getChar();
+                    return Tok_LeftShift;
+                }
                 return Tok_LeftAngleBracket;
             case '\'':
                 yyCh = getChar();

tests/auto/linguist/lupdate/testdata/good/parsecpp/main.cpp
@@ -756,3 +756,21 @@ class TestMetaStrings : QObject
         //@ label6
     }
 };
+
+namespace Core {
+struct Tr
+{
+    Q_DECLARE_TR_FUNCTIONS(Core)
+};
+}
+
+using namespace Core;
+class SaveItemsDialog {
+    void adjustButtonWidths();
+};
+
+void SaveItemsDialog::adjustButtonWidths()
+{
+    QStringList possibleTexts;
+    possibleTexts << Tr::tr("Save Selected");
+}

tests/auto/linguist/lupdate/testdata/good/parsecpp/project.ts.result
@@ -101,6 +101,14 @@ backslashed \ stuff.</source>
     </message>
 </context>
 <context>
+    <name>Core</name>
+    <message>
+        <location filename="main.cpp" line="775"/>
+        <source>Save Selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>Dialog2</name>
     <message numerus="yes">
         <location filename="main.cpp" line="57"/>