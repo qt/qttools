211d38cccf8be2b0876dc4ef3d9b1ab663fd3fc7
Paul Wicking <paul.wicking@qt.io>
2025-09-10 13:28:10 +0200

    QDoc: Add inclusion policy infrastructure
    QDoc has multiple functions that determine whether to include
    private nodes in documentation. These functions duplicate
    similar branching logic, creating maintenance challenges.
    
    This change introduces `InclusionFlags` and `InclusionPolicy` types to
    express private API inclusion rules uniformly. These foundational types
    enable centralized policy decisions in subsequent refactoring changes.
    
    Task-number: QTBUG-140077
    Change-Id: I8eca87cdb5aebcc46b11185ddc2716ff48e0d43a
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>

---
+++ src/qdoc/qdoc/src/qdoc/inclusionflags.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef INCLUSIONFLAGS_H
+#define INCLUSIONFLAGS_H
+
+#include <QtCore/QFlags>
+#include <QtCore/QtGlobal>
+
+QT_BEGIN_NAMESPACE
+
+enum class InclusionFlag : quint32 {
+    Private         = 0x0001,
+    PrivateFunction = 0x0002,
+    PrivateType     = 0x0004,  // Classes, Enums, Typedefs
+    PrivateVariable = 0x0008,
+};
+
+Q_DECLARE_FLAGS(InclusionFlags, InclusionFlag)
+Q_DECLARE_OPERATORS_FOR_FLAGS(InclusionFlags)
+
+QT_END_NAMESPACE
+
+#endif // INCLUSIONFLAGS_H
+
---
+++ src/qdoc/qdoc/src/qdoc/inclusionpolicy.h
@@ -0,0 +1,37 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef INCLUSIONPOLICY_H
+#define INCLUSIONPOLICY_H
+
+#include "inclusionflags.h"
+
+#include <QtCore/QtGlobal>
+
+QT_BEGIN_NAMESPACE
+
+struct InclusionPolicy
+{
+    bool includePrivate = false;
+    bool includePrivateFunction = false;
+    bool includePrivateType = false;
+    bool includePrivateVariable = false;
+
+    [[nodiscard]] InclusionFlags toFlags() const {
+        InclusionFlags flags {};
+        if (includePrivate)
+            flags |= InclusionFlag::Private;
+        if (includePrivateFunction)
+            flags |= InclusionFlag::PrivateFunction;
+        if (includePrivateType)
+            flags |= InclusionFlag::PrivateType;
+        if (includePrivateVariable)
+            flags |= InclusionFlag::PrivateVariable;
+        return flags;
+    }
+};
+
+QT_END_NAMESPACE
+
+#endif // INCLUSIONPOLICY_H
+
