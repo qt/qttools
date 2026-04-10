6c0e3e27559e320701f223d8e9d2018f8fa496a3
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-04-10 10:04:48 +0200

    Qt Widgets Designer: Suppress Axivion(SV570, SV586): Qt-Security-QProcessStart
    The warning about using relative paths for binaries is a false positive
    in both cases since the path is determined from
    QLibraryInfo::path(QLibraryInfo::BinariesPath) in both cases.
    
    Pick-to: 6.11
    Change-Id: I31b2cf98bfe53ac7338da445f1815185384e5781
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/shared/helpclient/assistantclient.cpp
+++ src/shared/helpclient/assistantclient.cpp
@@ -119,7 +119,11 @@ bool AssistantClient::ensureRunning(QString *errorMessage)
         qDebug() << "Running " << app;
     // run
     QStringList args{u"-enableRemoteControl"_s};
+
+    // AXIVION DISABLE Style Qt-Security-QProcessStart: False positive, path is absolute
     m_process->start(app, args);
+    // AXIVION ENABLE Style Qt-Security-QProcessStart
+
     if (!m_process->waitForStarted()) {
         *errorMessage = QCoreApplication::translate("AssistantClient", "Unable to launch assistant (%1).").arg(app);
         return false;
