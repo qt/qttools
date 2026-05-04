06229f16dd46669f1478d25e24aa60f182e2817d
Topi Reinio <topi.reinio@qt.io>
2026-05-04 11:45:22 +0000

    QDoc: Fix type string handling and \since support for QML enumerations
    Node::nodeTypeString() and Generator::typeString() were both missing
    a case for NodeType::QmlEnum. This caused QML enum nodes to return
    incorrect type strings (empty string and "documentation", respectively)
    instead of "enumeration".
    
    Extend the qmlenumvaluesfromcpp test to verify that the \since command
    works correctly for QML enumerations, and update expected output files
    accordingly.
    
    Fixes: QTBUG-146227
    Pick-to: 6.11
    Change-Id: I165be815281a0c5220a1f50ad3c2860553465c5b
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>

--- src/qdoc/qdoc/src/qdoc/generator.cpp
+++ src/qdoc/qdoc/src/qdoc/generator.cpp
@@ -2441,6 +2441,8 @@ QString Generator::typeString(const Node *node, bool plural)
         return plural ? "functions"_L1 : "function"_L1;
     }
     case NodeType::Property:
+    case NodeType::QmlEnum:
+        return plural ? "enumerations"_L1 : "enumeration"_L1;
     case NodeType::QmlProperty:
         return plural ? "properties"_L1 : "property"_L1;
     case NodeType::Module:
