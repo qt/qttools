50c3238aed664f75f332b50cfc1facd7ba403326
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-07-07 09:58:04 +0200

    Qt Widgets Designer: Mark as security-significant
    This excludes the shared sources rcc.cpp/_p.h which originate
    from the rcc tool in qtbase.
    
    A few risks were identified, but none of them justifies
    a higher level. Use default.
    
    QUIP: 23
    Task-number: QTBUG-147937
    Pick-to: 6.12 6.11 6.8
    Change-Id: I243d55c74fe7c27888c15a71243bcbadb3574eaa
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/designer/src/lib/shared/menuactionlineedit_p.h
+++ src/designer/src/lib/shared/menuactionlineedit_p.h
@@ -1,5 +1,7 @@
 // Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:significant reason:default
+
 //
 //  W A R N I N G
 //  -------------
