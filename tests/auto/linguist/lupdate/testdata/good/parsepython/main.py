b72e6d09e271d9957f98fac8cc07530cee3c4ad8
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-08-11 09:49:25 +0200

    lupdate: Fix up Python test
    - Minimize flake8 warnings (fix blank lines, imports)
    - Ensure the program runs and the (applicable) test slots are correct
    - Use modern QTranslator loading code
    - Amend 8d4ba0a8abbf93d82af6719db386f0a4d377e197 by
      - Replacing invalid self.translate() by QCoreApplication.translate()
      - Adding a separate test slot for self.tr()
    
    Task-number: PYSIDE-3418
    Task-number: QTBUG-100310
    Change-Id: I7b27032ceaeabd63f52513cf142c388deec0e5ee
    Reviewed-by: Masoud Jami <masoud.jami@qt.io>
    (cherry picked from commit b6dadb2342d2b3ed3d1e5b90b3dc8792aecedd4d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/linguist/lupdate/testdata/good/parsepython/main.py
+++ tests/auto/linguist/lupdate/testdata/good/parsepython/main.py
@@ -1,14 +1,18 @@
-from pathlib import Path
 import sys
 
-from PySide6.QtCore import (QItemSelection, QLibraryInfo, QLocale, QTranslator,
-                            Qt, Slot)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QListWidget,
-                               QMainWindow, QWidget)
-import linguist_rc
+from PySide6.QtCore import (QCoreApplication, QItemSelection, QLibraryInfo, QLocale, QTranslator,
+                            Slot)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QListWidget, QMainWindow)
+
+try:
+    import linguist_rc  # noqa: F401
+except ImportError:
+    pass
+
 
 """"
      Misleading indentation (5) for PYSIDE-2379"""
+
 
 class Window(QMainWindow):
     def __init__(self):
@@ -21,7 +25,7 @@ class Window(QMainWindow):
         quit_action.triggered.connect(self.close)
         help_menu = self.menuBar().addMenu(self.tr("&Help"))
         about_qt_action = help_menu.addAction(self.tr("About Qt"))
-        about_qt_action.triggered.connect(qApp.aboutQt)
+        about_qt_action.triggered.connect(qApp.aboutQt)  # noqa: F821
 
         self._list_widget = QListWidget()
         self._list_widget.setSelectionMode(QAbstractItemView.MultiSelection)
@@ -31,51 +35,52 @@ class Window(QMainWindow):
         self._list_widget.addItem("Python")
         self.setCentralWidget(self._list_widget)
 
+        # Run the test slots to verify syntactic correctness
+        self.selection_changed(QItemSelection(), QItemSelection())
+        self.test_tr_trailing_comma_behavior()
+        self.test_translate_trailing_comma_behavior()
+        self.window_method()
+        self.test_pyside2863()
+
     @Slot(QItemSelection, QItemSelection)
     def selection_changed(self, selected, deselected):
         count = len(self._list_widget.selectionModel().selectedRows())
         message = self.tr("%n language(s) selected", "", count)
         self.statusBar().showMessage(message)
 
+    def test_tr_trailing_comma_behavior(self):
+        self.tr("SOME TR TEXT, NO COMMENT, TRAILING COMMA",)
+        self.tr("SOME TR TEXT", "A COMMENT, NO TRAILING COMMA")
+        self.tr("EVEN MORE TR TEXT", "A COMMENT WITH PLURALIZATION", 42)
+        self.tr("YET MORE TR TEXT", "A COMMENT WITH PLURALIZATION AND A TRAILING COMMA", 42,)
+        count = 42
+        self.tr("EVEN MORE TR TEXT", "A COMMENT WITH PLURALIZATION/VARIABLE", count)
+        self.tr("YET MORE TR TEXT", "A COMMENT WITH PLURALIZATION/VARIABLE AND A TRAILING COMMA",
+                count,)
+
     def test_translate_trailing_comma_behavior(self):
-        self.translate("CONTEXT",
-                       "SOME TEXT, NO COMMENT, TRAILING COMMA",                  
-                      )
-        self.translate("CONTEXT",
-                       "SOME TEXT",
-                       "A COMMENT, NO TRAILING COMMA"
-                      )
-        self.translate("CONTEXT",
-                       "SOME MORE TEXT",
-                       "A COMMENT WITH A TRAILING COMMA",
-                      )
-        self.translate("CONTEXT",
-                       "EVEN MORE TEXT",
-                       "A COMMENT WITH PLURALIZATION",
-                       42
-                      )
-        self.translate("CONTEXT",
-                       "YET MORE TEXT",
-                       "A COMMENT WITH PLURALIZATION AND A TRAILING COMMA",
-                       42,
-                      )
-        self.translate("CONTEXT",
-                       "SOME TEXT, SHOULD NOT BE EXTRACTED",
-                       "A COMMENT WITH PLURALIZATION AND A TRAILING COMMA AND GARBAGE",
-                       42,
-                       "THIS SHOULD NOT WORK"
-                      )
+        QCoreApplication.translate("CONTEXT", "SOME TEXT, NO COMMENT, TRAILING COMMA",)
+        QCoreApplication.translate("CONTEXT", "SOME TEXT", "A COMMENT, NO TRAILING COMMA")
+        QCoreApplication.translate("CONTEXT", "SOME MORE TEXT", "A COMMENT WITH A TRAILING COMMA",)
+        QCoreApplication.translate("CONTEXT", "EVEN MORE TEXT", "A COMMENT WITH PLURALIZATION", 42)
+        QCoreApplication.translate("CONTEXT", "YET MORE TEXT",
+                                   "A COMMENT WITH PLURALIZATION AND A TRAILING COMMA", 42,)
+
+    def test_translate_fail(self):
+        QCoreApplication.translate("CONTEXT", "SOME TEXT, SHOULD NOT BE EXTRACTED",
+                                   "A COMMENT WITH PLURALIZATION AND A TRAILING COMMA AND GARBAGE",
+                                   42, "THIS SHOULD NOT WORK")
 
     class NestedClass:
         def foo(self):
-            msg = self.tr("Nested Message")
+            msg = self.tr("Nested Message")  # noqa: F841
 
     def window_method(self):  # PYSIDE-2379, Don't put this into NestedClass
         msg = self.tr("Window Message")
-        msg = self.tr(f"An f-string\\")
+        msg = self.tr(f"An f-string\\")  # noqa: F541
         msg = self.tr(r"A raw strin\g")
         msg = self.tr(r"A raw strin\g""continued\\")
-        msg = self.tr(r"A raw string with escaped quote\"bla")
+        msg = self.tr(r"A raw string with escaped quote\"bla")  # noqa: F841
 
     def test_pyside2863(self):
         """PYSIDE-2863, Check whether id and extra comments are correctly associated."""
@@ -90,18 +95,19 @@ class Window(QMainWindow):
         msg = self.tr("")
         msg = self.tr("prefix\u00A0\u00A0postfix")
         msg = self.tr("before\U000000A0middle\U000000A0after")
-        msg = self.tr("before\u00A0\U000000A0middle\x1F\U000000A0\u00A0after")
+        msg = self.tr("before\u00A0\U000000A0middle\x1F\U000000A0\u00A0after")  # noqa: F841
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    path = QLibraryInfo.location(QLibraryInfo.TranslationsPath)
     translator = QTranslator(app)
-    if translator.load(QLocale.system(), 'qtbase_', '', path):
-        app.installTranslator(translator)
+    for path in QLibraryInfo.paths(QLibraryInfo.LibraryPath.TranslationsPath):
+        if translator.load(QLocale(), 'qtbase', '_', path):
+            app.installTranslator(translator)
+            break
     translator = QTranslator(app)
-    path = ':/translations'
-    if translator.load(QLocale.system(), 'example_', '', path):
+    if translator.load(QLocale(), 'example', '_', ':/translations'):
         app.installTranslator(translator)
 
     window = Window()
