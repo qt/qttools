89f8a207b9a119eea39b6fd24f59bca28b909ddc
Topi Reinio <topi.reinio@qt.io>
2026-08-17 11:19:57 +0000

    QDoc: Fix logic in Generator::typeString()
    The switch case label for NodeType::Property must share scope with
    NodeType::QmlProperty, not QmlEnum.
    
    Amends 06229f16dd46669f1478d25e24aa60f182e2817d.
    
    Change-Id: I809e7d2337c76adb7e6fafa19e5ce3b00725a191
    Reviewed-by: Paul Wicking <paul.wicking@qt.io>
    (cherry picked from commit c55324a62b8aa8a342eaf0b233f603ee5a6b7c63)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 46b81d646d95649c37d4f81f4d2f6c1189bde47a)

--- src/qdoc/qdoc/src/qdoc/generator.cpp
+++ src/qdoc/qdoc/src/qdoc/generator.cpp
@@ -2326,9 +2326,9 @@ QString Generator::typeString(const Node *node, bool plural)
         }
         return plural ? "functions"_L1 : "function"_L1;
     }
-    case NodeType::Property:
     case NodeType::QmlEnum:
         return plural ? "enumerations"_L1 : "enumeration"_L1;
+    case NodeType::Property:
     case NodeType::QmlProperty:
         return plural ? "properties"_L1 : "property"_L1;
     case NodeType::Module:
